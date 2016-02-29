c360e814f1437ca654b0c03cdc2da93c507be74c
Dylan Marcus <dylan@mach1studios.com>
2016-02-29 18:22:00 -0500

    lang cs add

---
+++ lang/cs/Source/Mach1.AudioRouting.WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Mach1.AudioRouting.WPFClient
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}

61eaea8677c5e13c174caed06f363329b929ec9e
stav-ag <stav-ag@mail.ru>
2016-11-18 00:50:28 +0300

    UE4 add sound cube test

---
+++ Examples/Unreal Engine/CubeSoundTest/Source/CubeSoundTest/CubeSound.cpp
@@ -0,0 +1,374 @@
+//
+//  Mach1 SDK
+//
+#include "CubeSoundTest.h"
+#include "Kismet/HeadMountedDisplayFunctionLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "HeadMountedDisplay.h"
+
+#include "CubeSound.h" // Change to your project name!
+
+#include <sstream>
+#include "M1DSPAlgorithmsUE.h"
+
+#define MAX_SOUNDS_PER_CHANNEL 8
+#define MIN_SOUND_VOLUME (KINDA_SMALL_NUMBER*2)
+
+float ACubeSound::ClosestPointOnBox(FVector point, FVector center, FVector axis0, FVector axis1, FVector axis2, FVector extents, FVector & closestPoint)
+{
+	FVector vector = point - center;
+	float num = 0.0f;
+
+	float num0 = FVector::DotProduct(vector, axis0);
+	if (num0 < -extents.X)
+	{
+		num += powf(num0 + extents.X, 2);
+		num0 = -extents.X;
+	}
+	else if (num0 > extents.X)
+	{
+		num += powf(num0 - extents.X, 2);
+		num0 = extents.X;
+	}
+
+	float num1 = FVector::DotProduct(vector, axis1);
+	if (num1 < -extents.Y)
+	{
+		num += powf(num1 + extents.Y, 2);
+		num1 = -extents.Y;
+	}
+	else if (num1 > extents.Y)
+	{
+		num += powf(num1 - extents.Y, 2);
+		num1 = extents.Y;
+	}
+
+	float num2 = FVector::DotProduct(vector, axis2);
+	if (num2 < -extents.Z)
+	{
+		num += powf(num2 + extents.Z, 2);
+		num2 = -extents.Z;
+	}
+	else if (num2 > extents.Z)
+	{
+		num += powf(num2 - extents.Z, 2);
+		num2 = extents.Z;
+	}
+	closestPoint = center + num0 * axis0 + num1 * axis1 + num2 * axis2;
+
+	return sqrt(num);
+}
+
+
+// Sets default values
+ACubeSound::ACubeSound()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	Root->AttachTo(RootComponent);
+
+	Collision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+	Collision->InitBoxExtent(FVector(100, 100, 100));
+	Collision->bEditableWhenInherited = false;
+	Collision->SetHiddenInGame(false);
+	Collision->AttachTo(Root);
+
+	Billboard = CreateDefaultSubobject< UBillboardComponent>(TEXT("Billboard"));
+	Billboard->SetHiddenInGame(false);
+	Billboard->AttachTo(Root);
+
+	LeftChannels.SetNum(MAX_SOUNDS_PER_CHANNEL);
+	RightChannels.SetNum(MAX_SOUNDS_PER_CHANNEL);
+
+	Volume = 1;
+	for (int i = 0; i < MAX_SOUNDS_PER_CHANNEL * 2; i++) VolumeFactor.Add(1);
+}
+
+void ACubeSound::Init()
+{
+	if (!isInit)
+	{
+		USceneComponent* cameraComponent = nullptr;
+
+		if (APlayerController* player = GetWorld()->GetFirstPlayerController())//UGameplayStatics::GetPlayerCharacter(GetWorld(), 0))
+		{
+			TArray<UCameraComponent*> Cameras;
+			GetWorld()->GetFirstPlayerController()->GetPawn()->GetComponents<UCameraComponent>(Cameras);
+
+			if (Cameras.Num() == 0)
+			{
+				APlayerCameraManager* GameCameraManager = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0);
+				cameraComponent = GameCameraManager->GetRootComponent();
+			}
+			else
+			{
+				cameraComponent = Cameras[0];
+			}
+			//GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Purple, FString::FromInt(Cameras.Num()));
+
+			/*
+			AActor *vt = GetWorld()->GetFirstPlayerController()->GetViewTarget();
+			ACameraActor* camera = Cast<ACameraActor>(vt);
+			if (camera) {
+			GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Purple, camera->GetCameraComponent()->GetName());
+			}
+			*/
+		}
+
+		if (cameraComponent)
+		{
+			NullAttenuation = NewObject<USoundAttenuation>(this);
+			NullAttenuation->Attenuation.bAttenuate = false;
+			NullAttenuation->Attenuation.bSpatialize = true;
+
+			/*
+			if (GEngine->HMDDevice.IsValid() && !GEngine->HMDDevice->IsStereoEnabled())
+			{
+			FTransform transform;
+			transform.SetIdentity();
+
+			FQuat orientation;
+			FVector position;
+			GEngine->HMDDevice->GetCurrentOrientationAndPosition(orientation, position);
+			transform.SetRotation(orientation);
+			transform.SetTranslation(position);
+			}
+			*/
+
+			for (int i = 0; i < MAX_SOUNDS_PER_CHANNEL; i++)
+			{
+
+				LeftChannels[i] = NewObject <UAudioComponent>(cameraComponent, FName(*FString::Printf(TEXT("Left Channel %d"), i))); 
+				RightChannels[i] = NewObject <UAudioComponent>(cameraComponent, FName(*FString::Printf(TEXT("Right Channel %d"), i))); 
+
+				LeftChannels[i]->RegisterComponent(); // only for runtime
+				RightChannels[i]->RegisterComponent(); // only for runtime
+
+				LeftChannels[i]->SetRelativeLocation(FVector(0, -1, 0));
+				RightChannels[i]->SetRelativeLocation(FVector(0, 1, 0));
+
+				LeftChannels[i]->AttenuationSettings = NullAttenuation;
+				RightChannels[i]->AttenuationSettings = NullAttenuation;
+
+				LeftChannels[i]->AttachToComponent(cameraComponent, FAttachmentTransformRules::KeepRelativeTransform); // AttachToComponent(Root, FAttachmentTransformRules::KeepRelativeTransform);//   AttachTo(sceneComponent);
+				RightChannels[i]->AttachToComponent(cameraComponent, FAttachmentTransformRules::KeepRelativeTransform);
+			}
+
+			isInit = true;
+		}
+	}
+
+}
+
+
+void ACubeSound::SetSoundSet()
+{
+	if (isInit)
+	{
+		Sounds.Empty();
+
+		Sounds.Add(FrontUpLeftChannel);
+		Sounds.Add(FrontUpRightChannel);
+		Sounds.Add(BackUpLeftChannel);
+		Sounds.Add(BackUpRightChannel);
+		Sounds.Add(FrontDownLeftChannel);
+		Sounds.Add(FrontDownRightChannel);
+		Sounds.Add(BackDownLeftChannel);
+		Sounds.Add(BackDownRightChannel);
+
+		for (int i = 0; i < MAX_SOUNDS_PER_CHANNEL; i++)
+		{
+			LeftChannels[i]->SetSound(Sounds[i]);
+			RightChannels[i]->SetSound(Sounds[i]);
+		}
+	}
+
+}
+
+void ACubeSound::Play()
+{
+	if (isInit)
+	{
+		for (int i = 0; i < MAX_SOUNDS_PER_CHANNEL; i++)
+		{
+			LeftChannels[i]->Play();
+			RightChannels[i]->Play();
+		}
+	}
+}
+
+void ACubeSound::Stop()
+{
+	if (isInit)
+	{
+		for (int i = 0; i < MAX_SOUNDS_PER_CHANNEL; i++)
+		{
+			LeftChannels[i]->Stop();
+			RightChannels[i]->Stop();
+		}
+	}
+}
+
+// Called when the game starts or when spawned
+void ACubeSound::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (GEngine)
+	{
+		if (APlayerController* player = GetWorld()->GetFirstPlayerController())
+		{
+			Init();
+			SetSoundSet();
+			Play();
+		}
+	}
+}
+
+// Called every frame
+void ACubeSound::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (GEngine)
+	{
+		if (APlayerController* player = GetWorld()->GetFirstPlayerController())
+		{
+
+			/*
+			bool bHMDIsReady = (GEngine && GEngine->HMDDevice.IsValid() && GEngine->HMDDevice->IsHMDConnected());
+			if (bHMDIsReady)
+			{
+			GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Yellow, TEXT("HMD detected"));
+			FQuat DeviceRotation;
+			FVector  DevicePosition;
+			GEngine->HMDDevice->GetCurrentOrientationAndPosition(DeviceRotation, DevicePosition);
+			GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Yellow, TEXT(">> " + DeviceRotation.Euler().ToString()));
+			}
+
+			FRotator DeviceRotation;
+			FVector  DevicePosition;
+			UHeadMountedDisplayFunctionLibrary::GetOrientationAndPosition(DeviceRotation, DevicePosition);
+			GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Green, TEXT(">> " + DeviceRotation.Euler().ToString()));
+			*/
+
+			/*
+			GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Red, TEXT(">> " + player->GetControlRotation().Euler().ToString()));
+			GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Yellow, isInit ? TEXT("init ok") : TEXT("no init"));
+			GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Purple, GetWorld()->GetFirstPlayerController()->GetPawn()->GetName());
+			*/
+
+			FVector closestPoint = GetActorLocation();
+			if (ClosestPointOnBox(player->GetPawn()->GetActorLocation(), GetActorLocation(), GetActorRightVector(), GetActorUpVector(), GetActorForwardVector(), GetActorScale() * Collision->GetScaledBoxExtent() / 2, closestPoint) > 0)
+			{
+				DrawDebugLine(
+					GetWorld(),
+					GetActorLocation(),
+					closestPoint,
+					FColor(255, 0, 0),
+					false,
+					-1,
+					0,
+					0
+				);
+
+				DrawDebugPoint(GetWorld(),
+					closestPoint,
+					10.0,
+					FColor(255, 0, 0),
+					false,
+					-1,
+					0
+				);
+			}
+
+		
+		 	// Compute rotation for sound
+			FQuat quat = UKismetMathLibrary::FindLookAtRotation( player->GetPawn()->GetActorLocation(), closestPoint).Quaternion().Inverse() * GetActorRotation().Quaternion();
+			quat = FQuat::MakeFromEuler(FVector(useRoll ? quat.Euler().X : 0, usePitch ? quat.Euler().Y : 0, useYaw ? quat.Euler().Z : 0));
+			quat *= player->GetControlRotation().Quaternion();
+
+			
+			CalculateChannelVolumes(quat); 
+		}
+	}
+}
+
+void ACubeSound::PostEditChangeProperty(FPropertyChangedEvent & PropertyChangedEvent)
+{
+	if (PropertyChangedEvent.Property != NULL)
+	{
+		if (PropertyChangedEvent.Property->GetFName() == FName(TEXT("Debug")))
+		{
+			Collision->SetHiddenInGame(!Debug);
+			Billboard->SetHiddenInGame(!Debug);
+		}
+	}
+
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+
+template<typename T>
+std::string toDebugString(const T& value)
+{
+	std::ostringstream oss;
+	oss.precision(2);
+	oss << std::fixed << value;
+	return oss.str();
+}
+
+void ACubeSound::CalculateChannelVolumes(FQuat quat)
+{
+	//quat.Euler().X
+	std::vector<float> result = eightChannelsAlgorithm(quat.Euler().Y, quat.Euler().Z < 0 ? 360 + quat.Euler().Z : quat.Euler().Z, quat.Euler().X);
+
+	//#if UE_BUILD_DEBUG
+	std::string str = "angles: " + toDebugString(quat.Euler().Y) + " , " + toDebugString(quat.Euler().Z < 0 ? 360 + quat.Euler().Z : quat.Euler().Z) + " , " + toDebugString(quat.Euler().X);
+	GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Yellow, str.c_str());
+
+	 
+	std::string info;
+	info = "left:  ";
+	for (int i = 0; i < MAX_SOUNDS_PER_CHANNEL; i++)
+	{
+		info += toDebugString(result[i * 2]) + ", ";
+	}
+	GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Green, info.c_str());
+
+	info = "right: ";
+	for (int i = 0; i < MAX_SOUNDS_PER_CHANNEL; i++)
+	{
+		info += toDebugString(result[i * 2 + 1]) + ", ";
+	}
+	GEngine->AddOnScreenDebugMessage(-1, -1, FColor::Green, info.c_str());
+
+	//UE_LOG(LogTemp, Log, TEXT("Your message"));
+	//#endif
+
+	for (int i = 0; i < MAX_SOUNDS_PER_CHANNEL * 2; i++)
+		VolumeFactor[i] = result[i];
+
+	SetVolume(Volume);
+}
+
+void ACubeSound::SetVolume(float NewVolume)
+{
+	if (isInit)
+	{
+		Volume = FMath::Max(MIN_SOUND_VOLUME, NewVolume);
+
+		float NetVolume = 0;
+		for (int i = 0; i < MAX_SOUNDS_PER_CHANNEL; i++)
+		{
+			NetVolume = VolumeFactor[i * 2] * Volume;
+			NetVolume = FMath::Max(MIN_SOUND_VOLUME, NetVolume);
+			LeftChannels[i]->SetVolumeMultiplier(NetVolume);
+
+			NetVolume = VolumeFactor[i * 2 + 1] * Volume;
+			NetVolume = FMath::Max(MIN_SOUND_VOLUME, NetVolume);
+			RightChannels[i]->SetVolumeMultiplier(NetVolume);
+		}
+	}
+}
---
+++ Examples/Unreal Engine/CubeSoundTest/Source/CubeSoundTest/CubeSound.h
@@ -0,0 +1,94 @@
+//
+//  Mach1 SDK
+//
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "CubeSound.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class CUBESOUNDTEST_API ACubeSound : public AActor
+{
+	GENERATED_BODY()
+
+	static float ClosestPointOnBox(FVector point, FVector center, FVector axis0, FVector axis1, FVector axis2, FVector extents, FVector& closestPoint);
+ 
+	USoundAttenuation* NullAttenuation;
+	TArray<USoundBase*> Sounds;
+	TArray<float> VolumeFactor;
+
+	TArray<UAudioComponent*> LeftChannels;
+	TArray<UAudioComponent*> RightChannels;
+
+	USceneComponent* Root;
+	UBoxComponent* Collision;
+	UBillboardComponent* Billboard;
+
+	bool isInit;
+
+	void Init();
+	void SetSoundSet();
+	void CalculateChannelVolumes(FQuat quat);
+	void SetVolume(float NewVolume);
+
+public:
+	// Sets default values for this actor's properties
+	ACubeSound();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+
+	// always tick
+	bool ShouldTickIfViewportsOnly() const override { return true; }
+
+	void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName = "Debug")
+		bool Debug = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName = "Use Pitch")
+		bool usePitch = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName = "Use Yaw")
+		bool useYaw = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName = "Use Roll")
+		bool useRoll = true;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Sound")
+		float Volume;
+
+	UFUNCTION(BlueprintCallable, Category = "Audio")
+		void Play();
+
+	UFUNCTION(BlueprintCallable, Category = "Audio")
+		void Stop();
+
+	// Audio positions for the 8channel cube
+	UPROPERTY(EditAnywhere, Category = "Sound")
+		USoundBase* FrontUpLeftChannel;
+
+	UPROPERTY(EditAnywhere, Category = "Sound")
+		USoundBase* FrontUpRightChannel;
+
+	UPROPERTY(EditAnywhere, Category = "Sound")
+		USoundBase* BackUpLeftChannel;
+
+	UPROPERTY(EditAnywhere, Category = "Sound")
+		USoundBase* BackUpRightChannel;
+
+	UPROPERTY(EditAnywhere, Category = "Sound")
+		USoundBase* FrontDownLeftChannel;
+
+	UPROPERTY(EditAnywhere, Category = "Sound")
+		USoundBase* FrontDownRightChannel;
+
+	UPROPERTY(EditAnywhere, Category = "Sound")
+		USoundBase* BackDownLeftChannel;
+
+	UPROPERTY(EditAnywhere, Category = "Sound")
+		USoundBase* BackDownRightChannel;
+};

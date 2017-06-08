cee0df949c130040b4e1dc2ddc97dc4aa91f81e9
Dylan Marcus <dylan@mach1studios.com>
2017-06-08 15:23:49 -0400

    fmtconv: added m1spatial->surround

--- Utility/FMT_Conv/fmtconv_mac/src/MatrixConvert.h
+++ Utility/FMT_Conv/fmtconv_mac/src/MatrixConvert.h
@@ -380,6 +380,54 @@ private:
         { 0, 0, 0, r2o4, 0, 1, 0, r2o8 },
         { 0, 0, 0, 0, r2o4, 0, 1, r2o8 } };
     
+    // --- Surround to M1Spatial ---;
+    float Cube2FiveOneFilm[6][8] =
+    {   { 1, 0, 0, 0, 1, 0, 0, 0 },
+        { r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8 },
+        { 0, 1, 0, 0, 0, 1, 0, 0 },
+        { 0, 0, 1, 0, 0, 0, 1, 0 },
+        { 0, 0, 0, 1, 0, 0, 0, 1 },
+        { r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8 } };
+    float Cube2FiveOneFilm_Cinema[6][8] =
+    {   { 1, 0, 0, 0, 1, 0, 0, 0 },
+        { r2o4, r2o4, 0, 0, r2o4, r2o4, 0, 0 },
+        { 0, 1, 0, 0, 0, 1, 0, 0 },
+        { 0, 0, 1, 0, 0, 0, 1, 0 },
+        { 0, 0, 0, 1, 0, 0, 0, 1 },
+        { r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8 } };
+    float Cube2FiveOneSmpte[6][8] =
+    {   { 1, 0, 0, 0, 1, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 1, 0, 0 },
+        { r2o4, r2o4, 0, 0, r2o4, r2o4, 0, 0 },
+        { r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8 },
+        { 0, 0, 1, 0, 0, 0, 1, 0 },
+        { 0, 0, 0, 1, 0, 0, 0, 1 } };
+    float Cube2FiveOneDts[6][8] =
+    {   { 1, 0, 0, 0, 1, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 1, 0, 0 },
+        { 0, 0, 1, 0, 0, 0, 1, 0 },
+        { 0, 0, 0, 1, 0, 0, 0, 1 },
+        { r2o4, r2o4, 0, 0, r2o4, r2o4, 0, 0 },
+        { r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8 } };
+    float Cube2SevenOnePt[8][8] =
+    {   { 1, 0, 0, 0, 1, 0, 0, 0 },
+        { r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8 },
+        { 0, 1, 0, 0, 0, 1, 0, 0 },
+        { r2o4, 0, r2o4, 0, r2o4, 0, r2o4, 0 },
+        { 0, r2o4, 0, r2o4, 0, r2o4, 0, r2o4 },
+        { 0, 0, 1, 0, 0, 0, 1, 0 },
+        { 0, 0, 0, 1, 0, 0, 0, 1 },
+        { r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8 } };
+    float Cube2SevenOnePt_Cinema[8][8] =
+    {   { 1, 0, 0, 0, 1, 0, 0, 0 },
+        { r2o4, r2o4, 0, 0, r2o4, r2o4, 0, 0 },
+        { 0, 1, 0, 0, 0, 1, 0, 0 },
+        { r2o4, 0, r2o4, 0, r2o4, 0, r2o4, 0 },
+        { 0, r2o4, 0, r2o4, 0, r2o4, 0, r2o4 },
+        { 0, 0, 1, 0, 0, 0, 1, 0 },
+        { 0, 0, 0, 1, 0, 0, 0, 1 },
+        { r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8, r2o8 } };
+    
 	int fmtChannels[NUMFMTS] = { 4, 4, 4, 6, 8, 8, 10, 16, 2, 3, 5, 6, 6, 6, 6, 6, 8, 8, 9, 9 };
 	float* fmtMatrix[NUMFMTS][NUMFMTS] =
     {   { NULL, &(FuMa2ACNSN3D[0][0]), NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
@@ -392,7 +440,7 @@ private:
         
         { NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
         
-        { &(Cube2FuMa[0][0]), &(Cube2ACNSN3D[0][0]), &(Cube2Square[0][0]), NULL, &(Cube2Square8[0][0]), NULL, NULL, &(Cube2Cube16[0][0]), NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, &(Cube2ACNSN3DO2A[0][0]), &(Cube2ACNSN3DO2A[0][0]) },
+        { &(Cube2FuMa[0][0]), &(Cube2ACNSN3D[0][0]), &(Cube2Square[0][0]), NULL, &(Cube2Square8[0][0]), NULL, NULL, &(Cube2Cube16[0][0]), NULL, &(Cube2FiveOneFilm[0][0]), &(Cube2FiveOneFilm_Cinema[0][0]), &(Cube2FiveOneSmpte[0][0]), &(Cube2FiveOneDts[0][0]), NULL, &(Cube2SevenOnePt[0][0]), &(Cube2SevenOnePt_Cinema[0][0]), &(Cube2ACNSN3DO2A[0][0]), &(Cube2ACNSN3DO2A[0][0]) },
         
         { &(CubeS2FuMa[0][0]), &(CubeS2ACNSN3D[0][0]), &(CubeS2Square[0][0]), NULL, &(CubeS2Square8[0][0]), &(CubeS2Cube[0][0]), NULL, &(CubeS2Cube16[0][0]), NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL },
         

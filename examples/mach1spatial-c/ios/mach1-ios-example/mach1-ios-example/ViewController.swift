8090e771055c2ce22391a7337c6de5b29c929f2f
Dylan Marcus <dylan@mach1studios.com>
2018-02-23 17:20:29 -0500

    ios init

---
+++ examples/mach1spatial-c/ios/mach1-ios-example/mach1-ios-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mach1-ios-example
+//
+//  Created by Dylan Marcus on 2/19/18.
+//  Copyright © 2018 Mach1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ examples/mach1spatial-c/ios/mach1-ios-example/mach1-ios-exampleTests/mach1_ios_exampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  mach1_ios_exampleTests.swift
+//  mach1-ios-exampleTests
+//
+//  Created by Dylan Marcus on 2/19/18.
+//  Copyright © 2018 Mach1. All rights reserved.
+//
+
+import XCTest
+@testable import mach1_ios_example
+
+class mach1_ios_exampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

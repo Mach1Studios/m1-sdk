ef2a156606f5b116a06bdd337a0fd70749635e7f
Dylan Marcus <dylan@mach1studios.com>
2018-04-03 13:17:39 -0400

    ios: updated ios example and added README

---
+++ examples/mach1spatial-c/ios/README.md
@@ -0,0 +1,11 @@
+## Setup
+This example uses AudioKit to playback multiple channels of a streamed audio file.
+Ideally you will want to decide on your preffered way to stream audio to the Mach1Decode lib depending on whether you application supports
+multichannel audio files or not. 
+
+- Please download AudioKit
+`http://github.com/audiokit/AudioKit/releases/download/v4.1/AudioKit-iOS-4.1.zip`
+
+- Import `AudioKit.framework` into the Solution Browser of your project in Xcode.
+
+
--- examples/mach1spatial-c/ios/mach1-ios-example/mach1-ios-example/ViewController.swift
+++ examples/mach1spatial-c/ios/mach1-ios-example/mach1-ios-example/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreMotion
 import AVFoundation
+import AudioKit
 
 var motionManager = CMMotionManager()
 var stereoPlayer = AVAudioPlayer()
@@ -20,58 +21,42 @@ var isYawActive = true
 var isPitchActive = false
 var isRollActive = false
 
+var players: [AKPlayer] = []
+
+var mixer: AKMixer = AKMixer()
+
 class ViewController: UIViewController {
+    
+    
     
     @IBOutlet weak var yaw: UILabel!
     @IBOutlet weak var pitch: UILabel!
     @IBOutlet weak var roll: UILabel!
     @IBAction func playButton(_ sender: Any) {
-        if (!m1LPlayer0.isPlaying){
-            let shortStartDelay:TimeInterval = 0.1
-            m1LPlayer0.play(atTime: m1LPlayer0.deviceCurrentTime + shortStartDelay)
-            m1RPlayer0.play(atTime: m1RPlayer0.deviceCurrentTime + shortStartDelay)
-            m1LPlayer1.play(atTime: m1LPlayer1.deviceCurrentTime + shortStartDelay)
-            m1RPlayer1.play(atTime: m1RPlayer1.deviceCurrentTime + shortStartDelay)
-            m1LPlayer2.play(atTime: m1LPlayer2.deviceCurrentTime + shortStartDelay)
-            m1RPlayer2.play(atTime: m1RPlayer2.deviceCurrentTime + shortStartDelay)
-            m1LPlayer3.play(atTime: m1LPlayer3.deviceCurrentTime + shortStartDelay)
-            m1RPlayer3.play(atTime: m1RPlayer3.deviceCurrentTime + shortStartDelay)
-            m1LPlayer4.play(atTime: m1LPlayer4.deviceCurrentTime + shortStartDelay)
-            m1RPlayer4.play(atTime: m1RPlayer4.deviceCurrentTime + shortStartDelay)
-            m1LPlayer5.play(atTime: m1LPlayer5.deviceCurrentTime + shortStartDelay)
-            m1RPlayer5.play(atTime: m1RPlayer5.deviceCurrentTime + shortStartDelay)
-            m1LPlayer6.play(atTime: m1LPlayer6.deviceCurrentTime + shortStartDelay)
-            m1RPlayer6.play(atTime: m1RPlayer6.deviceCurrentTime + shortStartDelay)
-            m1LPlayer7.play(atTime: m1LPlayer7.deviceCurrentTime + shortStartDelay)
-            m1RPlayer7.play(atTime: m1RPlayer7.deviceCurrentTime + shortStartDelay)
-            
-            stereoPlayer.play()
-            print("isPlaying")
-        }else{
+        //        if (!m1LPlayer0.isPlaying){
+        let shortStartDelay:TimeInterval = 0.1
+        
+        
+        var startTime = AVAudioTime.now() + 0.25
+        for audioPlayer in players {
+            audioPlayer.play(at: startTime)
         }
+        
+        
+        
+        //stereoPlayer.play()
+        print("isPlaying")
+        //        }else{
+        //        }
     }
     @IBAction func stopButton(_ sender: Any) {
-        if (m1LPlayer0.isPlaying){
-            m1LPlayer0.pause()
-            m1RPlayer0.pause()
-            m1LPlayer1.pause()
-            m1RPlayer1.pause()
-            m1LPlayer2.pause()
-            m1RPlayer2.pause()
-            m1LPlayer3.pause()
-            m1RPlayer3.pause()
-            m1LPlayer4.pause()
-            m1RPlayer4.pause()
-            m1LPlayer5.pause()
-            m1RPlayer5.pause()
-            m1LPlayer6.pause()
-            m1RPlayer6.pause()
-            m1LPlayer7.pause()
-            m1RPlayer7.pause()
-            
-            stereoPlayer.pause()
-        } else {
+        for i in 0...7 {
+            do {
+                players[i * 2].stop()
+                players[i * 2 + 1].stop()
+            }
         }
+        stereoPlayer.pause()
     }
     @IBAction func staticStereoActive(_ sender: Any) {
         stereoActive = !stereoActive
@@ -91,77 +76,38 @@ class ViewController: UIViewController {
         
         //TODO: load multichannel audio instead
         //            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Guitar-8ch", ofType: "wav")!))
+        
+        mixer = AKMixer()
+        
+        mixer.volume = 1.0
+        AudioKit.output = mixer
+        
         do {
-            m1LPlayer0 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "000", ofType: "aif")!))
-            m1RPlayer0 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "000", ofType: "aif")!))
-            m1LPlayer1 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "001", ofType: "aif")!))
-            m1RPlayer1 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "001", ofType: "aif")!))
-            m1LPlayer2 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "002", ofType: "aif")!))
-            m1RPlayer2 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "002", ofType: "aif")!))
-            m1LPlayer3 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "003", ofType: "aif")!))
-            m1RPlayer3 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "003", ofType: "aif")!))
-            m1LPlayer4 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "004", ofType: "aif")!))
-            m1RPlayer4 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "004", ofType: "aif")!))
-            m1LPlayer5 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "005", ofType: "aif")!))
-            m1RPlayer5 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "005", ofType: "aif")!))
-            m1LPlayer6 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "006", ofType: "aif")!))
-            m1RPlayer6 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "006", ofType: "aif")!))
-            m1LPlayer7 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "007", ofType: "aif")!))
-            m1RPlayer7 = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "007", ofType: "aif")!))
+            try AudioKit.start()
         } catch {
             print (error)
         }
         
-        m1LPlayer0.prepareToPlay()
-        m1RPlayer0.prepareToPlay()
-        m1LPlayer1.prepareToPlay()
-        m1RPlayer1.prepareToPlay()
-        m1LPlayer2.prepareToPlay()
-        m1RPlayer2.prepareToPlay()
-        m1LPlayer3.prepareToPlay()
-        m1RPlayer3.prepareToPlay()
-        m1LPlayer4.prepareToPlay()
-        m1RPlayer4.prepareToPlay()
-        m1LPlayer5.prepareToPlay()
-        m1RPlayer5.prepareToPlay()
-        m1LPlayer6.prepareToPlay()
-        m1RPlayer6.prepareToPlay()
-        m1LPlayer7.prepareToPlay()
-        m1RPlayer7.prepareToPlay()
+        do {
+            for i in 0...7 {
+                players.append(try AKPlayer(url: URL.init(fileURLWithPath: Bundle.main.path(forResource: "00" + String(i), ofType: "aif")!))!)
+                players.append(try AKPlayer(url: URL.init(fileURLWithPath: Bundle.main.path(forResource: "00" + String(i), ofType: "aif")!))!)
+                
+                players[i * 2].isLooping = true
+                players[i * 2 + 1].isLooping = true
+                
+                players[i * 2].pan = -1.0;
+                players[i * 2 + 1].pan = 1.0;
+                
+                
+                players[i * 2].outputNode.connect(to: mixer.inputNode)
+                players[i * 2 + 1].outputNode.connect(to: mixer.inputNode)
+            }
+            
+        } catch {
+            print (error)
+        }
         
-        m1LPlayer0.pan = -1.0;
-        m1RPlayer0.pan = 1.0;
-        m1LPlayer1.pan = -1.0;
-        m1RPlayer1.pan = 1.0;
-        m1LPlayer2.pan = -1.0;
-        m1RPlayer2.pan = 1.0;
-        m1LPlayer3.pan = -1.0;
-        m1RPlayer3.pan = 1.0;
-        m1LPlayer4.pan = -1.0;
-        m1RPlayer4.pan = 1.0;
-        m1LPlayer5.pan = -1.0;
-        m1RPlayer5.pan = 1.0;
-        m1LPlayer6.pan = -1.0;
-        m1RPlayer6.pan = 1.0;
-        m1LPlayer7.pan = -1.0;
-        m1RPlayer7.pan = 1.0;
-        
-        print(m1LPlayer0)
-        print(m1RPlayer0)
-        print(m1LPlayer1)
-        print(m1RPlayer1)
-        print(m1LPlayer2)
-        print(m1RPlayer2)
-        print(m1LPlayer3)
-        print(m1RPlayer3)
-        print(m1LPlayer4)
-        print(m1RPlayer4)
-        print(m1LPlayer5)
-        print(m1RPlayer5)
-        print(m1LPlayer6)
-        print(m1RPlayer6)
-        print(m1LPlayer7)
-        print(m1RPlayer7)
         
         //Static Stereo
         do{
@@ -194,9 +140,9 @@ class ViewController: UIViewController {
                     //Device orientation management
                     let deviceYaw = attitude.yaw * 180/M_PI
                     let devicePitch = attitude.pitch * 180/M_PI
-                    //                        let devicePitch = 0.0
+                    //                    let devicePitch = 0.0
                     let deviceRoll = attitude.roll * 180/M_PI
-                    //                        let deviceRoll = 0.0
+                    //                    let deviceRoll = 0.0
                     //                    print("Yaw: ", deviceYaw)
                     //                    print("Pitch: ", devicePitch)
                     DispatchQueue.main.async() {
@@ -216,40 +162,16 @@ class ViewController: UIViewController {
                     //                    print(decodeArray)
                     
                     //Use each coeff to decode multichannel Mach1 Spatial mix
-                    m1LPlayer0.volume = decodeArray[0]
-                    m1RPlayer0.volume = decodeArray[1]
-                    m1LPlayer1.volume = decodeArray[2]
-                    m1RPlayer1.volume = decodeArray[3]
-                    m1LPlayer2.volume = decodeArray[4]
-                    m1RPlayer2.volume = decodeArray[5]
-                    m1LPlayer3.volume = decodeArray[6]
-                    m1RPlayer3.volume = decodeArray[7]
-                    m1LPlayer4.volume = decodeArray[8]
-                    m1RPlayer4.volume = decodeArray[9]
-                    m1LPlayer5.volume = decodeArray[10]
-                    m1RPlayer5.volume = decodeArray[11]
-                    m1LPlayer6.volume = decodeArray[12]
-                    m1RPlayer6.volume = decodeArray[13]
-                    m1LPlayer7.volume = decodeArray[14]
-                    m1RPlayer7.volume = decodeArray[15]
+                    for i in 0...7 {
+                        players[i * 2].volume = Double(decodeArray[i * 2])
+                        players[i * 2 + 1].volume = Double(decodeArray[i * 2 + 1])
+                        
+                        print(String(players[i * 2].currentTime) + " ; " + String(i * 2))
+                        print(String(players[i * 2 + 1].currentTime) + " ; " + String(i * 2 + 1))
+                    }
                     
                     //print coeffs
-                    print("channel 0 L: ", m1LPlayer0.volume)
-                    print("channel 0 R: ", m1RPlayer0.volume)
-                    print("channel 1 L: ", m1LPlayer1.volume)
-                    print("channel 1 R: ", m1RPlayer1.volume)
-                    print("channel 2 L: ", m1LPlayer2.volume)
-                    print("channel 2 R: ", m1RPlayer2.volume)
-                    print("channel 3 L: ", m1LPlayer3.volume)
-                    print("channel 3 R: ", m1RPlayer3.volume)
-                    print("channel 4 L: ", m1LPlayer4.volume)
-                    print("channel 4 R: ", m1RPlayer4.volume)
-                    print("channel 5 L: ", m1LPlayer5.volume)
-                    print("channel 5 R: ", m1RPlayer5.volume)
-                    print("channel 6 L: ", m1LPlayer6.volume)
-                    print("channel 6 R: ", m1RPlayer6.volume)
-                    print("channel 7 L: ", m1LPlayer7.volume)
-                    print("channel 7 R: ", m1RPlayer7.volume)
+                    
                 }
             })
             print("Device motion started")
@@ -260,3 +182,4 @@ class ViewController: UIViewController {
     }
     
 }
+

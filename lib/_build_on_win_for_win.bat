81169d539b08690ff8f0d28f925767b539ca45be
Dylan_Mach1 <dylan@mach1studios.com>
2018-04-10 18:39:26 -0400

    win: updated bins

--- lib/_build_on_win_for_win.bat
+++ lib/_build_on_win_for_win.bat
@@ -1,5 +1,7 @@
-python ../../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-14-2015
-python ../../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-14-2015-win64
+python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-14-2015
+python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-14-2015-win64
+python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-15-2017
+python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-15-2017-win64
 
 del /f /s /q _win 1>nul
 rmdir /s /q _win
@@ -7,11 +9,16 @@ rmdir /s /q _win
 mkdir _win
 cd _win
 
-mkdir x86
-mkdir x64
+mkdir -p vs14\x86
+mkdir -p vs14\x64
+mkdir -p vs15\x86
+mkdir -p vs15\x64
 
 cd %~dp0
 
-xcopy _install\vs-14-2015\lib _win\x86
-xcopy _install\vs-14-2015-win64\lib _win\x64
+xcopy _install\vs-14-2015\lib _win\vs14\x86
+xcopy _install\vs-14-2015-win64\lib _win\vs14\x64
+xcopy _install\vs-15-2017\lib _win\vs15\x86
+xcopy _install\vs-15-2017-win64\lib _win\vs15\x64
+
 pause

e0e87ce06f2139f6adea8baa2d8cd7d1df055fc4
Dylan Marcus <dylan@mach1studios.com>
2018-05-31 19:39:50 -0400

    ios: capi fix

--- examples/mach1spatial-c/ios/mach1-ios-example/mach1-ios-example/Mach1DecodeCAPI.h
+++ examples/mach1spatial-c/ios/mach1-ios-example/mach1-ios-example/Mach1DecodeCAPI.h
@@ -29,12 +29,12 @@ struct Mach1Point3D {
     float x, y, z;
 };
 
-enum Mach1AngularSettingsType {
-    m1Default = 0, m1Unity, m1UE, m1oFEasyCam, m1Android, m1iOSPortrait, m1iOSLandscape
+enum Mach1PlatformType {
+    Mach1PlatformDefault = 0, Mach1PlatformUnity, Mach1PlatformUE, Mach1PlatformOfEasyCam, Mach1PlatformAndroid, Mach1PlatformiOSPortrait, Mach1PlatformiOSLandscape
 };
 
-enum Mach1AlgorithmType {
-    m1Spatial = 0, m1AltSpatial, m1Horizon, m1HorizonPairs, m1SpatialPairs
+enum Mach1DecodeAlgoType {
+    Mach1DecodeAlgoSpatial = 0, Mach1DecodeAlgoAltSpatial, Mach1DecodeAlgoHorizon, Mach1DecodeAlgoHorizonPairs, Mach1DecodeAlgoSpatialPairs
 };
 
 #ifdef __cplusplus
@@ -43,8 +43,8 @@ extern "C" {
 	M1_API void* Mach1DecodeCAPI_create();
 	M1_API void Mach1DecodeCAPI_delete(void* M1obj);
     
-    M1_API void Mach1DecodeCAPI_setAngularSettingsType(void* M1obj, int type);
-    M1_API void Mach1DecodeCAPI_setAlgorithmType(void* M1obj, int newAlgorithmType);
+    M1_API void Mach1DecodeCAPI_setDecodeAlgoType(void* M1obj, int newAlgorithmType);
+    M1_API void Mach1DecodeCAPI_setPlatformType(void* M1obj, int type);
 
     M1_API void Mach1DecodeCAPI_decode(void* M1obj, float Yaw, float Pitch, float Roll, float *result, int bufferSize, int sampleIndex);
 

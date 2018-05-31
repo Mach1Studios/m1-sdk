32ffffc41868bae9ed53c5eee35aa0ddffc189e8
Sasha <stav-ag@mail.ru>
2018-05-31 23:07:04 +0300

    sdk. remove unity external (use c# instead), remove processing external (use java instead)

--- lib/external/java/Mach1Decode.java
+++ lib/external/java/Mach1Decode.java
@@ -1,75 +1,52 @@
 //  Mach1 SDK
 //  Copyright © 2017 Mach1. All rights reserved.
 
+package com.Mach1;
+
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.NativeLong;
 import com.sun.jna.Platform;
 import com.sun.jna.*;
 
-public interface Mach1DecodeCAPI extends Library {
-  Pointer Mach1DecodeCAPI_create();
-  void Mach1DecodeCAPI_delete(Pointer M1obj);
-
-  Pointer Mach1DecodeCAPI_horizonAlgo(Pointer M1obj, float Yaw, float Pitch, float Roll, boolean smoothAngles);
-  Pointer Mach1DecodeCAPI_horizonPairsAlgo(Pointer M1obj, float Yaw, float Pitch, float Roll, boolean smoothAngles);
-  Pointer Mach1DecodeCAPI_spatialAlgo(Pointer M1obj, float Yaw, float Pitch, float Roll, boolean smoothAngles);
-  Pointer Mach1DecodeCAPI_spatialAltAlgo(Pointer M1obj, float Yaw, float Pitch, float Roll, boolean smoothAngles);
-  Pointer Mach1DecodeCAPI_spatialPairsAlgo(Pointer M1obj, float Yaw, float Pitch, float Roll, boolean smoothAngles);
-
-  long Mach1DecodeCAPI_getCurrentTime(Pointer M1obj);
-}
-
-public static class Mach1Decode {
+public class Mach1Decode {
   private Pointer M1obj;
+  private interface Mach1DecodeCAPI extends Library {
+    Pointer Mach1DecodeCAPI_create();
+    void Mach1DecodeCAPI_delete(Pointer M1obj);
+
+    void Mach1DecodeCAPI_decode(Pointer M1obj, float Yaw, float Pitch, float Roll, Pointer data, int bufferSize, int sampleIndex);
+
+    long Mach1DecodeCAPI_getCurrentTime(Pointer M1obj);
+  }
   private Mach1DecodeCAPI M1API;
 
-  Mach1Decode(String dir)
+  public Mach1Decode(String dir)
   {
     if (M1API == null)
     {
-      M1API = (Mach1DecodeCAPI)Native.loadLibrary(dir + (Platform.isWindows() ? "/Mach1DecodeCAPI.dll" : "/Mach1DecodeCAPI.so"), Mach1DecodeCAPI.class);
+      M1API = (Mach1DecodeCAPI)Native.loadLibrary(dir + (Platform.isWindows() ? "Mach1DecodeCAPI.dll" : "Mach1DecodeCAPI.so"), Mach1DecodeCAPI.class);
     }
     M1obj = M1API.Mach1DecodeCAPI_create();
-  }
-
-  public float[] horizonAlgo(float Yaw, float Pitch, float Roll, boolean smoothAngles)
-  {
-    Pointer p = M1API.Mach1DecodeCAPI_horizonAlgo(M1obj, Yaw, Pitch, Roll, smoothAngles);
-    return p.getFloatArray(0, 10);
-  }
-
-  public float[] horizonPairsAlgo(float Yaw, float Pitch, float Roll, boolean smoothAngles)
-  {
-    Pointer p = M1API.Mach1DecodeCAPI_horizonPairsAlgo(M1obj, Yaw, Pitch, Roll, smoothAngles);
-    return p.getFloatArray(0, 8);
-  }
-
-
-  public float[] spatialAlgo(float Yaw, float Pitch, float Roll, boolean smoothAngles)
-  {
-    Pointer p = M1API.Mach1DecodeCAPI_spatialAlgo(M1obj, Yaw, Pitch, Roll, smoothAngles);
-    return p.getFloatArray(0, 18);
-  }
-
-  public float[] spatialAltAlgo(float Yaw, float Pitch, float Roll, boolean smoothAngles)
-  {
-    Pointer p = M1API.Mach1DecodeCAPI_spatialAltAlgo(M1obj, Yaw, Pitch, Roll, smoothAngles);
-    return p.getFloatArray(0, 18);
-  }
-
-  public float[] spatialPairsAlgo(float Yaw, float Pitch, float Roll, boolean smoothAngles)
-  {
-    Pointer p = M1API.Mach1DecodeCAPI_spatialPairsAlgo(M1obj, Yaw, Pitch, Roll, smoothAngles);
-    return p.getFloatArray(0, 16);
-  }
-
-  public long getCurrentTime()
-  {
-    return M1API.Mach1DecodeCAPI_getCurrentTime(M1obj);
   }
 
   protected void finalize ( ) {
     M1API.Mach1DecodeCAPI_delete(M1obj);
   }
+
+  public void decode(float Yaw, float Pitch, float Roll, float[] data, int bufferSize, int sampleIndex)
+  {
+    Pointer ptr = new Memory(data.length * Float.SIZE);
+    ptr.write(0, data, 0, data.length); //<>//
+
+    M1API.Mach1DecodeCAPI_decode(M1obj, Yaw, Pitch, Roll, ptr, bufferSize, sampleIndex);
+
+    System.arraycopy(ptr.getFloatArray(0, 18), 0, data, 0, 18);
+  }
+ 
+  public long getCurrentTime()
+  {
+    return M1API.Mach1DecodeCAPI_getCurrentTime(M1obj);
+  }
+
 }

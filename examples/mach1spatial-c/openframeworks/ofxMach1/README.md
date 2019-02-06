1d28303a7478f480e6d9092ba0f1a7ae0a8d812f
Dylan Marcus <dylan@mach1studios.com>
2019-02-06 00:20:19 -0500

    lib: fixed cmake a bit, switched all binaries to static, added all binaries everywhere (untested), fixed ofxMach1 for linking, fixed READMEs of openframework's examples

--- examples/mach1spatial-c/openframeworks/ofxMach1/README.md
+++ examples/mach1spatial-c/openframeworks/ofxMach1/README.md
@@ -1,12 +1,17 @@
+### Mach1 C++ Demonstrations
+### 
+### 
+### Current Demos:
+ - M1-Decode Example: Decoding example with variable device conditions and update rates 
+ - M1-Encode Example: Encoding example with visual GUI
+ - M1-AudioPlayer: Additional Decoding example
 
-* On OS X, you must have this in the Run Script Build Phase of your Xcode project. where the first path ../../../addons/ofxMach1/ is the path to the ofxMach1 addon.
-
-```
-rsync -aved  "$OF_PATH/addons/ofxMach1/libs/lib/osx/libMach1DecodeCAPI.dylib" "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/Frameworks/";
-install_name_tool -change libMach1DecodeCAPI.dylib @executable_path/../Frameworks/libMach1DecodeCAPI.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/$PRODUCT_NAME";
-
-rsync -aved  "$OF_PATH/addons/ofxMach1/libs/lib/osx/libMach1EncodeCAPI.dylib" "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/Frameworks/";
-install_name_tool -change libMach1EncodeCAPI.dylib @executable_path/../Frameworks/libMach1EncodeCAPI.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/$PRODUCT_NAME";
-```
-
-If you don't have this you'll see an error in the console: dyld: Library not loaded: libMach1DecodeCAPI.dylib
+#### Build Instructions
+ - Download: http://openframeworks.cc/versions/v0.10.1/
+ - Download dependencies (varies per demo): 
+	- ofxAudioDecoder: https://github.com/kylemcdonald/ofxAudioDecoder
+	- ofxImGui: https://github.com/jvcleave/ofxImGui
+- Move ofxMach1 to your addons directory in OpenFrameworks
+- Use ProjectGenerator App pointed to repo directory to create Xcode file 
+- After a successful build, copy your 8x mono channels to Resources/[1][2][3]/ for audio playback testing
+- Ensure to hardcode the serial port for your controller if using an IMU
--- examples/mach1spatial-c/openframeworks/ofxMach1/addon_config.mk
+++ examples/mach1spatial-c/openframeworks/ofxMach1/addon_config.mk
@@ -1,11 +1,88 @@
+# All variables and this file are optional, if they are not present the PG and the
+# makefiles will try to parse the correct values from the file system.
+#
+# Variables that specify exclusions can use % as a wildcard to specify that anything in
+# that position will match. A partial path can also be specified to, for example, exclude
+# a whole folder from the parsed paths from the file system
+#
+# Variables can be specified using = or +=
+# = will clear the contents of that variable both specified from the file or the ones parsed
+# from the file system
+# += will add the values to the previous ones in the file or the ones parsed from the file
+# system
+#
+# The PG can be used to detect errors in this file, just create a new project with this addon
+# and the PG will write to the console the kind of error and in which line it is
+
 meta:
 	ADDON_NAME = ofxMach1
-	ADDON_DESCRIPTION = Addon for decoding Mach1 Spatial
+	ADDON_DESCRIPTION = Addon for Mach1 Spatial VVBP(SPS)
 	ADDON_AUTHOR = Mach1
-	ADDON_TAGS =
-	ADDON_URL = http://www.mach1studios.com
+	ADDON_TAGS = "spatial audio" "spatial functions"
+	ADDON_URL = http://dev.mach1.xyz
 
 common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	# ADDON_DEPENDENCIES =
+
+	# include search paths, this will be usually parsed from the file system
+	# but if the addon or addon libraries need special search paths they can be
+	# specified here separated by spaces or one per line using +=
+	# ADDON_INCLUDES =
+
+	# any special flag that should be passed to the compiler when using this
+	# addon
+	# ADDON_CFLAGS =
+
+	# any special flag that should be passed to the linker when using this
+	# addon, also used for system libraries with -lname
+	# ADDON_LDFLAGS =
+
+	# linux only, any library that should be included in the project using
+	# pkg-config
+	# ADDON_PKG_CONFIG_LIBRARIES =
+
+	# osx/iOS only, any framework that should be included in the project
+	# ADDON_FRAMEWORKS =
+
+	# source files, these will be usually parsed from the file system looking
+	# in the src folders in libs and the root of the addon. if your addon needs
+	# to include files in different places or a different set of files per platform
+	# they can be specified here
+	# ADDON_SOURCES =
+
+	# some addons need resources to be copied to the bin/data folder of the project
+	# specify here any files that need to be copied, you can use wildcards like * and ?
+	# ADDON_DATA =
+
+	# when parsing the file system looking for libraries exclude this for all or
+	# a specific platform
+	# ADDON_LIBS_EXCLUDE =
+
+linux:
+linux64:
+linuxarmv6l:
+linuxarmv7l:
+android/armeabi-v7a:
+	ADDON_LIBS += libs/lib/android/armeabi-v7a/libMach1DecodeCAPI.a
+	ADDON_LIBS += libs/lib/android/armeabi-v7a/libMach1EncodeCAPI.a
+	ADDON_LIBS += libs/lib/android/armeabi-v7a/libMach1DecodePositionalCAPI.a
+android/x86:
+	ADDON_LIBS += libs/lib/android/x86/libMach1DecodeCAPI.a
+	ADDON_LIBS += libs/lib/android/x86/libMach1EncodeCAPI.a
+	ADDON_LIBS += libs/lib/android/x86/libMach1DecodePositionalCAPI.a
 osx:
+	ADDON_LIBS += libs/lib/osx/libMach1DecodeCAPI.a
+	ADDON_LIBS += libs/lib/osx/libMach1EncodeCAPI.a
+	ADDON_LIBS += libs/lib/osx/libMach1DecodePositionalCAPI.a
+ios:
+	ADDON_LIBS += libs/lib/ios/libMach1DecodeCAPI.a
+	ADDON_LIBS += libs/lib/ios/libMach1EncodeCAPI.a
+	ADDON_LIBS += libs/lib/ios/libMach1DecodePositionalCAPI.a
+tvos:
+msys2:
 vs:
-	ADDON_LIBS += libs/lib/win64/Mach1DecodeCAPI.lib
+	ADDON_LIBS += libs/lib/vs/x64/Mach1DecodeCAPI.lib
+	ADDON_LIBS += libs/lib/vs/x64/Mach1EncodeCAPI.lib
+	ADDON_LIBS += libs/lib/vs/x64/Mach1DecodePositionalCAPI.lib

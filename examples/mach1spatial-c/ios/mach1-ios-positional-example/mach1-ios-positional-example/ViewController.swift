000986d32ec0429abc270f7b9f8b2944eaf4164a
Dylan Marcus <dylan@mach1studios.com>
2019-02-13 12:12:16 -0500

    ios: fixed includes, added positional example for ios

---
+++ examples/mach1spatial-c/ios/mach1-ios-positional-example/mach1-ios-positional-example/ViewController.swift
@@ -0,0 +1,232 @@
+//
+//  ViewController.swift
+//  mach1-ios-example
+//
+//  Created by Dylan Marcus on 2/19/18.
+//  Copyright © 2018 Mach1. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+import AVFoundation
+
+var motionManager = CMMotionManager()
+var stereoPlayer = AVAudioPlayer()
+var m1obj = Mach1DecodePositional()
+var stereoActive = false
+var isYawActive = true
+var isPitchActive = false
+var isRollActive = false
+var isPlaying = false
+
+private var audioEngine: AVAudioEngine = AVAudioEngine()
+private var mixer: AVAudioMixerNode = AVAudioMixerNode()
+var players: [AVAudioPlayer] = []
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var yaw: UILabel!
+    @IBOutlet weak var pitch: UILabel!
+    @IBOutlet weak var roll: UILabel!
+    @IBAction func playButton(_ sender: Any) {
+        if !isPlaying {
+            var startDelayTime = 1.0
+            var now = players[0].deviceCurrentTime
+            var startTime = now + startDelayTime
+            print (startTime)
+            for audioPlayer in players {
+                audioPlayer.play(atTime: startTime)
+            }
+            //stereoPlayer.play()
+            print("isPlaying")
+            isPlaying = true
+        }
+    }
+    @IBAction func stopButton(_ sender: Any) {
+        for audioPlayer in players {
+            audioPlayer.stop()
+        }
+        isPlaying = false
+        //stereoPlayer.stop()
+        // prep files for next play
+        for i in 0...7 {
+            players[i * 2].prepareToPlay()
+            players[i * 2 + 1].prepareToPlay()
+        }
+        //stereoPlayer.prepareToPlay()
+    }
+    @IBAction func staticStereoActive(_ sender: Any) {
+        stereoActive = !stereoActive
+    }
+    @IBAction func yawActive(_ sender: Any) {
+        isYawActive = !isYawActive
+    }
+    @IBAction func pitchActive(_ sender: Any) {
+        isPitchActive = !isPitchActive
+    }
+    @IBAction func rollActive(_ sender: Any) {
+        isRollActive = !isRollActive
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        do {
+            for i in 0...7 {
+                //load in the individual streams of audio from a Mach1 Spatial encoded audio file
+                //this example assumes you have decoded the multichannel (8channel) audio file into individual streams
+                players.append(try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "00" + String(i), ofType: "aif")!)))
+                players.append(try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "00" + String(i), ofType: "aif")!)))
+                
+                players[i * 2].numberOfLoops = 10
+                players[i * 2 + 1].numberOfLoops = 10
+                
+                //the Mach1Decode function 8*2 channels to correctly recreate the stereo image
+                players[i * 2].pan = -1.0
+                players[i * 2 + 1].pan = 1.0
+                
+                players[i * 2].prepareToPlay()
+                players[i * 2 + 1].prepareToPlay()
+                
+            }
+            
+            //Mach1 Decode Setup
+            //Setup the correct angle convention for orientation Euler input angles
+            m1obj.setPlatformType(type: Mach1PlatformiOSLandscape)
+            //Setup the expected spatial audio mix format for decoding
+            m1obj.setDecodeAlgoType(newAlgorithmType: Mach1DecodeAlgoSpatial)
+            //Setup for the safety filter speed:
+            //1.0 = no filter | 0.1 = slow filter
+            m1obj.setFilterSpeed(filterSpeed: 1.0)
+            
+            //Mach1 Decode Positional Setup
+            //Advanced Setting: used for blending 2 m1obj for crafting room ambiences
+            m1obj.setUseBlendMode(useBlendMode: false)
+            //Advanced Setting: ignore movements on height plane
+            m1obj.setIgnoreTopBottom(ignoreTopBottom: false)
+            //Setting: mute audio when setCameraPosition position is outside of m1obj volume
+            //based on setDecoderAlgoPosition & setDecoderAlgoScale
+            m1obj.setMuteWhenOutsideObject(muteWhenOutsideObject: false)
+            //Setting: mute audio when setCameraPosition position is inside of m1obj volume
+            //based on setDecoderAlgoPosition & setDecoderAlgoScale
+            m1obj.setMuteWhenInsideObject(muteWhenInsideObject: false)
+            //Setting: turn on/off distance attenuation of m1obj
+            m1obj.setUseFalloff(useFalloff: true)
+            //Advanced Setting: when on, positional rotation is calculated from the closest point
+            //of the m1obj's volume and not rotation from the center of m1obj.
+            //use this if you want the positional rotation tracking to be from a plane instead of from a point
+            m1obj.setUseClosestPointRotationMuteInside(bool: false)
+            //Setting: on/off yaw rotations from position
+            m1obj.setUseYawForRotation(bool: true)
+            //Setting: on/off pitch rotations from position
+            m1obj.setUsePitchForRotation(bool: false)
+            //Setting: on/off roll rotations from position
+            m1obj.setUseRollForRotation(bool: false)
+        } catch {
+            print (error)
+        }
+        
+        
+        //Static Stereo
+        do{
+            stereoPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "stereo", ofType: "wav")!))
+        } catch {
+            print(error)
+        }
+        stereoPlayer.prepareToPlay()
+        print(stereoPlayer)
+        
+        //TODO: split audio channels for independent coeffs from our lib
+        //let channelCount = audioPlayer.numberOfChannels
+        //print("Channel Count: ", channelCount)
+        
+        //Allow audio to play when app closes
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+        } catch {
+            print(error)
+        }
+        
+        // Ensure to keep a strong reference to the motion manager otherwise you won't get updates
+        if motionManager.isDeviceMotionAvailable == true {
+            motionManager.deviceMotionUpdateInterval = 0.01;
+            let queue = OperationQueue()
+            motionManager.startDeviceMotionUpdates(to: queue, withHandler: { [weak self] (motion, error) -> Void in
+                
+                // Get the attitudes of the device
+                let attitude = motion?.attitude
+                //Device orientation management
+                var cameraYaw : Float = Float(attitude!.yaw) * 180 / Float.pi
+                var cameraPitch : Float = Float(attitude!.pitch) * 180 / Float.pi
+                var cameraRoll : Float = Float(attitude!.roll) * 180 / Float.pi
+                //                    print("Yaw: ", deviceYaw)
+                //                    print("Pitch: ", devicePitch)
+
+                // Please notice that you're expected to correct the correct the angles you get from
+                // the device's sensors to provide M1 Library with accurate angles in accordance to documentation.
+                // (documentation URL here)
+                switch UIDevice.current.orientation{
+                    case .portrait:
+                        cameraYaw += 90
+                        cameraPitch -= 90
+                    case .portraitUpsideDown:
+                        cameraYaw -= 90
+                        cameraPitch += 90
+                    case .landscapeLeft:
+                        cameraRoll += 90
+                    case .landscapeRight:
+                        cameraYaw += 180
+                        cameraRoll -= 90
+//                    default:
+                    
+                    default: break
+                    //
+                }
+                
+                DispatchQueue.main.async() {
+                    self?.yaw.text = String(cameraYaw)
+                    self?.pitch.text = String(cameraPitch)
+                    self?.roll.text = String(cameraRoll)
+                }
+                //Mute stereo if off
+                if (stereoActive) {
+                    stereoPlayer.setVolume(1.0, fadeDuration: 0.1)
+                } else if (!stereoActive) {
+                    stereoPlayer.setVolume(0.0, fadeDuration: 0.1)
+                }
+                
+                //Send device orientation to m1obj with the preferred algo
+                
+                
+                m1obj.setCameraPosition(point: Mach1Point3D(x: 0, y: 0, z: 0))
+                m1obj.setCameraRotation(point: Mach1Point3D(x: cameraYaw, y: cameraPitch, z: cameraRoll))
+                m1obj.setDecoderAlgoPosition(point: Mach1Point3D(x: 0, y: 0, z: 0))
+                m1obj.setDecoderAlgoRotation(point: Mach1Point3D(x: 0, y: 0, z: 0))
+                m1obj.setDecoderAlgoScale(point: Mach1Point3D(x: 1, y: 1, z: 1))
+                m1obj.evaluatePositionResults()
+                
+                var decodeArray: [Float] = Array(repeating: 0.0, count: 18)
+                m1obj.getVolumesRoom(result: &decodeArray)
+                
+                //                    print(decodeArray)
+                
+                //Use each coeff to decode multichannel Mach1 Spatial mix
+                for i in 0...7 {
+                    players[i * 2].setVolume(Float(decodeArray[i * 2]), fadeDuration: 0)
+                    players[i * 2 + 1].setVolume(Float(decodeArray[i * 2 + 1]), fadeDuration: 0)
+                    
+                    print(String(players[i * 2].currentTime) + " ; " + String(i * 2))
+                    print(String(players[i * 2 + 1].currentTime) + " ; " + String(i * 2 + 1))
+                }
+                
+                
+            })
+            print("Device motion started")
+        } else {
+            print("Device motion unavailable");
+        }
+        
+    }
+    
+}
+
--- include/swift/Mach1DecodePositional.swift
+++ include/swift/Mach1DecodePositional.swift
@@ -1,12 +1,11 @@
 import Foundation
 
-class Mach1DecodePositionalCAPI {
+class Mach1DecodePositional {
     var M1obj : UnsafeMutableRawPointer
     
     init() {
         M1obj = Mach1DecodePositionalCAPI_create()
     }
-    
     deinit {
         Mach1DecodePositionalCAPI_delete(M1obj)
     }
@@ -14,103 +13,106 @@ class Mach1DecodePositionalCAPI {
     func setPlatformType(type: Mach1PlatformType) {
         Mach1DecodePositionalCAPI_setPlatformType(M1obj, type)
     }
-    
     func setDecodeAlgoType(newAlgorithmType: Mach1DecodeAlgoType) {
         Mach1DecodePositionalCAPI_setDecodeAlgoType(M1obj, newAlgorithmType)
     }
     
     // settings
     func setUseBlendMode(useBlendMode: Bool) {
-        Mach1DecodePositionalCAPI_setUseBlendMode(M1obj, useBlendMode);
+        Mach1DecodePositionalCAPI_setUseBlendMode(M1obj, useBlendMode)
     }
     func setIgnoreTopBottom(ignoreTopBottom: Bool) {
-        Mach1DecodePositionalCAPI_setIgnoreTopBottom(M1obj, ignoreTopBottom);
+        Mach1DecodePositionalCAPI_setIgnoreTopBottom(M1obj, ignoreTopBottom)
     }
     
     func setMuteWhenOutsideObject(muteWhenOutsideObject: Bool) {
-        Mach1DecodePositionalCAPI_setMuteWhenOutsideObject(M1obj, muteWhenOutsideObject);
+        Mach1DecodePositionalCAPI_setMuteWhenOutsideObject(M1obj, muteWhenOutsideObject)
     }
     
     func setMuteWhenInsideObject(muteWhenInsideObject: Bool) {
-        Mach1DecodePositionalCAPI_setMuteWhenInsideObject(M1obj, muteWhenInsideObject);
+        Mach1DecodePositionalCAPI_setMuteWhenInsideObject(M1obj, muteWhenInsideObject)
     }
     
     func setUseFalloff(useFalloff: Bool) {
-        Mach1DecodePositionalCAPI_setUseFalloff(M1obj, useFalloff);
+        Mach1DecodePositionalCAPI_setUseFalloff(M1obj, useFalloff)
     }
     func setFalloffCurve(falloffCurve: Float) {
-        Mach1DecodePositionalCAPI_setFalloffCurve(M1obj, falloffCurve);
+        Mach1DecodePositionalCAPI_setFalloffCurve(M1obj, falloffCurve)
     }
     func setFalloffCurveBlendMode(falloffCurveBlendMode: Float) {
-        Mach1DecodePositionalCAPI_setFalloffCurveBlendMode(M1obj, falloffCurveBlendMode);
+        Mach1DecodePositionalCAPI_setFalloffCurveBlendMode(M1obj, falloffCurveBlendMode)
     }
     
     func setUseClosestPointRotationMuteInside(bool useClosestPointRotationMuteInside: Bool) {
-        Mach1DecodePositionalCAPI_setUseClosestPointRotationMuteInside(M1obj, useClosestPointRotationMuteInside);
+        Mach1DecodePositionalCAPI_setUseClosestPointRotationMuteInside(M1obj, useClosestPointRotationMuteInside)
     }
     
     func setUseYawForRotation(bool useYawForRotation: Bool) {
-        Mach1DecodePositionalCAPI_setUseYawForRotation(M1obj, useYawForRotation);
+        Mach1DecodePositionalCAPI_setUseYawForRotation(M1obj, useYawForRotation)
         
     }
     func setUsePitchForRotation(bool usePitchForRotation: Bool) {
-        Mach1DecodePositionalCAPI_setUsePitchForRotation(M1obj, usePitchForRotation);
+        Mach1DecodePositionalCAPI_setUsePitchForRotation(M1obj, usePitchForRotation)
         
     }
     func setUseRollForRotation(bool useRollForRotation: Bool) {
-        Mach1DecodePositionalCAPI_setUseRollForRotation(M1obj, useRollForRotation);
+        Mach1DecodePositionalCAPI_setUseRollForRotation(M1obj, useRollForRotation)
         
     }
     
     // parameters for algo
     func setCameraPosition(point: Mach1Point3D) {
-        Mach1DecodePositionalCAPI_setCameraPosition(M1obj, point);
+        Mach1DecodePositionalCAPI_setCameraPosition(M1obj, point)
     }
     func setCameraRotation(point: Mach1Point3D) {
-        Mach1DecodePositionalCAPI_setCameraRotation(M1obj, point);
+        Mach1DecodePositionalCAPI_setCameraRotation(M1obj, point)
     }
     func setCameraRotationQuat(point: Mach1Point4D) {
-        Mach1DecodePositionalCAPI_setCameraRotationQuat(M1obj, point);
+        Mach1DecodePositionalCAPI_setCameraRotationQuat(M1obj, point)
     }
     func setDecoderAlgoPosition(point: Mach1Point3D) {
-        Mach1DecodePositionalCAPI_setDecoderAlgoPosition(M1obj, point);
-        
+        Mach1DecodePositionalCAPI_setDecoderAlgoPosition(M1obj, point)
     }
     func setDecoderAlgoRotation(point: Mach1Point3D) {
-        Mach1DecodePositionalCAPI_setDecoderAlgoRotation(M1obj, point);
+        Mach1DecodePositionalCAPI_setDecoderAlgoRotation(M1obj, point)
     }
     func setDecoderAlgoRotationQuat(point: Mach1Point4D) {
-        Mach1DecodePositionalCAPI_setDecoderAlgoRotationQuat(M1obj, point);
+        Mach1DecodePositionalCAPI_setDecoderAlgoRotationQuat(M1obj, point)
     }
     func setDecoderAlgoScale(point: Mach1Point3D) {
-        Mach1DecodePositionalCAPI_setDecoderAlgoScale(M1obj, point);
+        Mach1DecodePositionalCAPI_setDecoderAlgoScale(M1obj, point)
     }
     
-    func evaluatePostionResults() {
-        Mach1DecodePositionalCAPI_evaluatePostionResults(M1obj);
+    func evaluatePositionResults() {
+        Mach1DecodePositionalCAPI_evaluatePostionResults(M1obj)
     }
     
     func getVolumesWalls(result: inout [Float]) {
-        let pointer: UnsafeMutablePointer< Float > = UnsafeMutablePointer(&result);
+        let pointer: UnsafeMutablePointer< Float > = UnsafeMutablePointer(&result)
         Mach1DecodePositionalCAPI_getVolumesWalls(M1obj, pointer);
     }
     func getVolumesRoom(result: inout [Float]) {
-        let pointer: UnsafeMutablePointer< Float > = UnsafeMutablePointer(&result);
+        let pointer: UnsafeMutablePointer< Float > = UnsafeMutablePointer(&result)
         Mach1DecodePositionalCAPI_getVolumesRoom(M1obj, pointer);
     }
     func getDist() -> Float {
-        return Mach1DecodePositionalCAPI_getDist(M1obj);
+        return Mach1DecodePositionalCAPI_getDist(M1obj)
     }
-    func getCurrentAngle() -> struct Mach1Point3D {
-    return Mach1DecodePositionalCAPI_getCurrentAngle(M1obj);
+    
+    func getCurrentAngle() -> Mach1Point3D {
+        return Mach1DecodePositionalCAPI_getCurrentAngle(M1obj)
     }
-    func getVolumeRotation() -> struct Mach1Point3D {
-    return Mach1DecodePositionalCAPI_getVolumeRotation(M1obj);
+    
+    func getVolumeRotation() -> Mach1Point3D {
+        return Mach1DecodePositionalCAPI_getVolumeRotation(M1obj)
     }
+    
     func setFilterSpeed(filterSpeed: Float) {
         Mach1DecodePositionalCAPI_setFilterSpeed(M1obj, filterSpeed)
     }
+    
 }
+
 
 
 

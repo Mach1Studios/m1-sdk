36ccb2e5cf741c54aebe7cbac3ad1d31ccc8bea3
Dylan Marcus <dylan@mach1studios.com>
2019-03-08 16:49:54 -0500

    lib: patch 1.0.2.1

--- examples/mach1spatial-c/openframeworks/ofxMach1/libs/libmach1/include/Mach1DecodeCAPI.h
+++ examples/mach1spatial-c/openframeworks/ofxMach1/libs/libmach1/include/Mach1DecodeCAPI.h
@@ -7,6 +7,7 @@
 
 #include "Mach1Point3D.h"
 #include "Mach1Point4D.h"
+#include <stdbool.h>
 
 #ifdef Mach1DecodeCore_h
     #ifndef M1_API
@@ -31,7 +32,7 @@
 #define Mach1DecodeCAPI_h
 
 enum Mach1PlatformType {
-	Mach1PlatformDefault = 0, Mach1PlatformUnity, Mach1PlatformUE, Mach1PlatformOfEasyCam, Mach1PlatformAndroid, Mach1PlatformiOSPortrait, Mach1PlatformiOSLandscape
+	Mach1PlatformDefault = 0, Mach1PlatformUnity, Mach1PlatformUE, Mach1PlatformOfEasyCam, Mach1PlatformAndroid, Mach1PlatformiOS
 };
 
 enum Mach1DecodeAlgoType {
@@ -39,14 +40,16 @@ enum Mach1DecodeAlgoType {
 };
 
 
-extern "C" { 
+#ifdef __cplusplus
+extern "C" {
+#endif
 	M1_API void* Mach1DecodeCAPI_create();
 	M1_API void Mach1DecodeCAPI_delete(void* M1obj);
   
-	M1_API void Mach1DecodeCAPI_setDecodeAlgoType(void* M1obj, Mach1DecodeAlgoType algorithmType);
-	M1_API void Mach1DecodeCAPI_setPlatformType(void* M1obj, Mach1PlatformType platformType);
+	M1_API void Mach1DecodeCAPI_setDecodeAlgoType(void* M1obj, enum Mach1DecodeAlgoType algorithmType);
+	M1_API void Mach1DecodeCAPI_setPlatformType(void* M1obj, enum Mach1PlatformType platformType);
 
-	M1_API void Mach1DecodeCAPI_decode(void* M1obj, float Yaw, float Pitch, float Roll, float *result, int bufferSize = 0, int sampleIndex = 0);
+	M1_API void Mach1DecodeCAPI_decode(void* M1obj, float Yaw, float Pitch, float Roll, float *result, int bufferSize, int sampleIndex);
 
 	M1_API void Mach1DecodeCAPI_setFilterSpeed(void* M1obj, float filterSpeed);
 	M1_API void Mach1DecodeCAPI_beginBuffer(void* M1obj);
@@ -55,7 +58,9 @@ extern "C" {
 	M1_API long Mach1DecodeCAPI_getCurrentTime(void* M1obj);
 	M1_API char* Mach1DecodeCAPI_getLog(void * M1obj);
 
-	M1_API Mach1Point3D Mach1DecodeCAPI_getCurrentAngle(void* M1obj);
+	M1_API struct Mach1Point3D Mach1DecodeCAPI_getCurrentAngle(void* M1obj);
+#ifdef __cplusplus
 }
+#endif
 
 #endif 

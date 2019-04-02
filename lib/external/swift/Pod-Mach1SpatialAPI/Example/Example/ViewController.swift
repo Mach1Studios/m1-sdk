427b84ab6da9b66e599d14bc192224fb460fe849
Sasha <stav-ag@mail.ru>
2019-04-02 19:18:39 +0300

    pods. fixed errors in pod file

---
+++ lib/external/swift/Pod-Mach1SpatialAPI/Example/Example/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by User on 02/04/2019.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import UIKit
+import Mach1SpatialAPI
+
+//var m1obj = Mach1SpatialAPI.Mach1DecodePositional()
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ lib/external/swift/Pod-Mach1SpatialAPI/Mach1SpatialAPI/Classes/Mach1Decode.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+class Mach1Decode {
+    var M1obj : UnsafeMutableRawPointer
+    
+    init() {
+        M1obj = Mach1DecodeCAPI_create()
+    }
+    
+    deinit {
+        Mach1DecodeCAPI_delete(M1obj)
+    }
+    
+    func setPlatformType(type: Mach1PlatformType) {
+        Mach1DecodeCAPI_setPlatformType(M1obj, type)
+    }
+    
+    func setDecodeAlgoType(newAlgorithmType: Mach1DecodeAlgoType) {
+        Mach1DecodeCAPI_setDecodeAlgoType(M1obj, newAlgorithmType)
+    }
+
+    func setFilterSpeed(filterSpeed: Float) {
+        Mach1DecodeCAPI_setFilterSpeed(M1obj, filterSpeed)
+    }
+    
+    func beginBuffer() {
+        Mach1DecodeCAPI_beginBuffer(M1obj)
+    }
+    
+    func endBuffer() {
+        Mach1DecodeCAPI_endBuffer(M1obj)
+    }
+    
+    func getCurrentTime() -> Int {
+        let t = Mach1DecodeCAPI_getCurrentTime(M1obj)
+        return t
+    }
+    
+    func getLog() -> String {
+        let str = String(cString: UnsafePointer(Mach1DecodeCAPI_getLog(M1obj)))
+        return str
+    }
+    
+    func decode(Yaw: Float, Pitch: Float, Roll: Float, bufferSize: Int = 0, sampleIndex: Int = 0) -> [Float] {
+        var array: [Float] = Array(repeating: 0.0, count: 18)
+        Mach1DecodeCAPI_decode(M1obj, Yaw, Pitch, Roll, &array, CInt(bufferSize), CInt(sampleIndex))
+        return array
+    }
+   
+}
+

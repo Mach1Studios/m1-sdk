5a5a5cdb29a7bddaa4311b3378081d06ff2a9dae
Dylan Marcus <dylan@mach1studios.com>
2019-04-09 14:12:53 -0400

    ios & unity updates

--- include/swift/Mach1Decode.swift
+++ include/swift/Mach1Decode.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-class Mach1Decode {
+public class Mach1Decode {
     var M1obj : UnsafeMutableRawPointer
     
-    init() {
+    public init() {
         M1obj = Mach1DecodeCAPI_create()
     }
     
@@ -11,37 +11,37 @@ class Mach1Decode {
         Mach1DecodeCAPI_delete(M1obj)
     }
     
-    func setPlatformType(type: Mach1PlatformType) {
+    public func setPlatformType(type: Mach1PlatformType) {
         Mach1DecodeCAPI_setPlatformType(M1obj, type)
     }
     
-    func setDecodeAlgoType(newAlgorithmType: Mach1DecodeAlgoType) {
+    public func setDecodeAlgoType(newAlgorithmType: Mach1DecodeAlgoType) {
         Mach1DecodeCAPI_setDecodeAlgoType(M1obj, newAlgorithmType)
     }
 
-    func setFilterSpeed(filterSpeed: Float) {
+    public func setFilterSpeed(filterSpeed: Float) {
         Mach1DecodeCAPI_setFilterSpeed(M1obj, filterSpeed)
     }
     
-    func beginBuffer() {
+    public func beginBuffer() {
         Mach1DecodeCAPI_beginBuffer(M1obj)
     }
     
-    func endBuffer() {
+    public func endBuffer() {
         Mach1DecodeCAPI_endBuffer(M1obj)
     }
     
-    func getCurrentTime() -> Int {
+    public func getCurrentTime() -> Int {
         let t = Mach1DecodeCAPI_getCurrentTime(M1obj)
         return t
     }
     
-    func getLog() -> String {
+    public func getLog() -> String {
         let str = String(cString: UnsafePointer(Mach1DecodeCAPI_getLog(M1obj)))
         return str
     }
     
-    func decode(Yaw: Float, Pitch: Float, Roll: Float, bufferSize: Int = 0, sampleIndex: Int = 0) -> [Float] {
+    public func decode(Yaw: Float, Pitch: Float, Roll: Float, bufferSize: Int = 0, sampleIndex: Int = 0) -> [Float] {
         var array: [Float] = Array(repeating: 0.0, count: 18)
         Mach1DecodeCAPI_decode(M1obj, Yaw, Pitch, Roll, &array, CInt(bufferSize), CInt(sampleIndex))
         return array

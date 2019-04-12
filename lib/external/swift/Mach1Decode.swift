d02160d063f79342e3f3891ee1a8e9a0e29526e1
Dylan Marcus <dylan@mach1studios.com>
2019-04-12 18:51:08 -0400

    swift: more commenting to includes

--- lib/external/swift/Mach1Decode.swift
+++ lib/external/swift/Mach1Decode.swift
@@ -13,22 +13,47 @@ public class Mach1Decode {
     
     public func setPlatformType(type: Mach1PlatformType) {
         Mach1DecodeCAPI_setPlatformType(M1obj, type)
+        /// Set the device's angle order and convention if applicable
+        ///
+        /// - Parameters:
+        ///     - Mach1PlatformDefault = 0
+        ///     - Mach1PlatformUnity = 1
+        ///     - Mach1PlatformUE = 2
+        ///     - Mach1PlatformOfEasyCam = 3
+        ///     - Mach1PlatformAndroid = 4
+        ///     - Mach1PlatformiOS = 5
     }
     
     public func setDecodeAlgoType(newAlgorithmType: Mach1DecodeAlgoType) {
         Mach1DecodeCAPI_setDecodeAlgoType(M1obj, newAlgorithmType)
+        /// Set the decoding algorithm
+        ///
+        /// - Parameters:
+        ///     - Mach1DecodeAlgoSpatial = 0 (default spatial | 8 channels)
+        ///     - Mach1DecodeAlgoAltSpatial = 1 (periphonic spatial | 8 channels)
+        ///     - Mach1DecodeAlgoHorizon = 2 (compass / yaw | 4 channels)
+        ///     - Mach1DecodeAlgoHorizonPairs = 3 (compass / yaw | 4x stereo mastered pairs)
+        ///     - Mach1DecodeAlgoSpatialPairs = 4 (experimental periphonic pairs | 8x stereo mastered pairs)
     }
 
     public func setFilterSpeed(filterSpeed: Float) {
         Mach1DecodeCAPI_setFilterSpeed(M1obj, filterSpeed)
+        /// Filter speed determines the amount of angle smoothing applied 
+        /// to the orientation angles used for the Mach1DecodeCore class
+        ///
+        /// - Parameters: 
+        ///     - value range: 0.0001 -> 1.0 (where 0.1 would be a slow filter
+        ///     and 1.0 is no filter)
     }
     
     public func beginBuffer() {
         Mach1DecodeCAPI_beginBuffer(M1obj)
+        /// Call this function before reading from the Mach1Decode buffer
     }
     
     public func endBuffer() {
         Mach1DecodeCAPI_endBuffer(M1obj)
+        /// Call this function after reading from the Mach1Decode buffer
     }
     
     public func getCurrentTime() -> Int {
@@ -45,6 +70,8 @@ public class Mach1Decode {
         var array: [Float] = Array(repeating: 0.0, count: 18)
         Mach1DecodeCAPI_decode(M1obj, Yaw, Pitch, Roll, &array, CInt(bufferSize), CInt(sampleIndex))
         return array
+        /// Call with current update's angles to return the resulting coefficients
+        /// to apply to the audioplayer's volume
     }
    
 }
--- lib/external/swift/Mach1DecodePositional.swift
+++ lib/external/swift/Mach1DecodePositional.swift
@@ -6,41 +6,74 @@ public class Mach1DecodePositional {
     public init() {
         M1obj = Mach1DecodePositionalCAPI_create()
     }
+
     deinit {
         Mach1DecodePositionalCAPI_delete(M1obj)
     }
     
     public func setPlatformType(type: Mach1PlatformType) {
         Mach1DecodePositionalCAPI_setPlatformType(M1obj, type)
+        /// Set the device's angle order and convention if applicable
+        ///
+        /// - Parameters:
+        ///     - Mach1PlatformDefault = 0
+        ///     - Mach1PlatformUnity = 1
+        ///     - Mach1PlatformUE = 2
+        ///     - Mach1PlatformOfEasyCam = 3
+        ///     - Mach1PlatformAndroid = 4
+        ///     - Mach1PlatformiOS = 5
     }
     public func setDecodeAlgoType(newAlgorithmType: Mach1DecodeAlgoType) {
         Mach1DecodePositionalCAPI_setDecodeAlgoType(M1obj, newAlgorithmType)
+        /// Set the decoding algorithm
+        ///
+        /// - Parameters:
+        ///     - Mach1DecodeAlgoSpatial = 0 (default spatial | 8 channels)
+        ///     - Mach1DecodeAlgoAltSpatial = 1 (periphonic spatial | 8 channels)
+        ///     - Mach1DecodeAlgoHorizon = 2 (compass / yaw | 4 channels)
+        ///     - Mach1DecodeAlgoHorizonPairs = 3 (compass / yaw | 4x stereo mastered pairs)
+        ///     - Mach1DecodeAlgoSpatialPairs = 4 (experimental periphonic pairs | 8x stereo mastered pairs)
     }
     
-    // settings
     public func setUseBlendMode(useBlendMode: Bool) {
         Mach1DecodePositionalCAPI_setUseBlendMode(M1obj, useBlendMode)
+        /// Warning: Experimental feature
+        /// Remark: Part of BlendMode
     }
+
     public func setIgnoreTopBottom(ignoreTopBottom: Bool) {
         Mach1DecodePositionalCAPI_setIgnoreTopBottom(M1obj, ignoreTopBottom)
+        /// Warning: Experimental feature
+        /// Remark: Part of BlendMode
     }
     
     public func setMuteWhenOutsideObject(muteWhenOutsideObject: Bool) {
         Mach1DecodePositionalCAPI_setMuteWhenOutsideObject(M1obj, muteWhenOutsideObject)
+        /// Mute mach1decode object (all coefficifient results becomes 0) 
+        /// when outside the positional reference shape/point
     }
     
     public func setMuteWhenInsideObject(muteWhenInsideObject: Bool) {
         Mach1DecodePositionalCAPI_setMuteWhenInsideObject(M1obj, muteWhenInsideObject)
+        /// Mute mach1decode object (all coefficifient results becomes 0) 
+        /// when inside the positional reference shape/point
     }
     
     public func setUseFalloff(useFalloff: Bool) {
         Mach1DecodePositionalCAPI_setUseFalloff(M1obj, useFalloff)
+        /// Set distance attenuation calculations on that mach1decode object on/off
+        ///
+        /// Remark: When off, distance remains 1.0
     }
+
     public func setFalloffCurve(falloffCurve: Float) {
         Mach1DecodePositionalCAPI_setFalloffCurve(M1obj, falloffCurve)
     }
+
     public func setFalloffCurveBlendMode(falloffCurveBlendMode: Float) {
         Mach1DecodePositionalCAPI_setFalloffCurveBlendMode(M1obj, falloffCurveBlendMode)
+        /// Warning: Experimental feature
+        /// Remark: Part of BlendMode
     }
     
     public func setUseClosestPointRotationMuteInside(bool useClosestPointRotationMuteInside: Bool) {
@@ -49,15 +82,23 @@ public class Mach1DecodePositional {
     
     public func setUseYawForRotation(bool useYawForRotation: Bool) {
         Mach1DecodePositionalCAPI_setUseYawForRotation(M1obj, useYawForRotation)
-        
+        /// Set use/ignore Yaw angle rotation results from pivoting positionally
+        ///
+        /// Remark: Default is true
     }
+
     public func setUsePitchForRotation(bool usePitchForRotation: Bool) {
         Mach1DecodePositionalCAPI_setUsePitchForRotation(M1obj, usePitchForRotation)
-        
+        /// Set use/ignore Pitch angle rotation results from pivoting positionally
+        ///
+        /// Remark: Default is true
     }
+
     public func setUseRollForRotation(bool useRollForRotation: Bool) {
         Mach1DecodePositionalCAPI_setUseRollForRotation(M1obj, useRollForRotation)
-        
+        /// Set use/ignore Roll angle rotation results from pivoting positionally
+        ///
+        /// Remark: Default is true
     }
     
     public func setCameraPosition(point: Mach1Point3D) {
@@ -67,6 +108,7 @@ public class Mach1DecodePositional {
         /// - Parameters:
         ///     - Mach1Point3D Position: x,y,z
     }
+
     public func setCameraRotation(point: Mach1Point3D) {
         Mach1DecodePositionalCAPI_setCameraRotation(M1obj, point)
         /// Sets the device/camera's orientation with Euler angles
@@ -74,6 +116,7 @@ public class Mach1DecodePositional {
         /// - Parameters:
         ///     - Mach1Point3D Rotation: yaw,pitch,roll
     }
+
     public func setCameraRotationQuat(point: Mach1Point4D) {
         Mach1DecodePositionalCAPI_setCameraRotationQuat(M1obj, point)
         /// Sets the device/camera's orientation with Quaternion
@@ -81,6 +124,7 @@ public class Mach1DecodePositional {
         /// - Parameters:
         ///     - Mach1Point4D Rotation: Quaternion
     }
+
     public func setDecoderAlgoPosition(point: Mach1Point3D) {
         Mach1DecodePositionalCAPI_setDecoderAlgoPosition(M1obj, point)
         /// Sets the decode object's position in desired x,y,z space
@@ -88,6 +132,7 @@ public class Mach1DecodePositional {
         /// - Parameters:
         ///     - Mach1Point3D Rotation: x,y,z
     }
+
     public func setDecoderAlgoRotation(point: Mach1Point3D) {
         Mach1DecodePositionalCAPI_setDecoderAlgoRotation(M1obj, point)
         /// Sets the decode object's orientation with Euler angles
@@ -95,6 +140,7 @@ public class Mach1DecodePositional {
         /// - Parameters:
         ///     - Mach1Point3D Rotation: yaw,pitch,roll
     }
+
     public func setDecoderAlgoRotationQuat(point: Mach1Point4D) {
         Mach1DecodePositionalCAPI_setDecoderAlgoRotationQuat(M1obj, point)
         /// Sets the decode object's orientation with Quaternion
@@ -102,6 +148,7 @@ public class Mach1DecodePositional {
         /// - Parameters:
         ///     - Mach1Point4D Rotation: Quaternion
     }
+
     public func setDecoderAlgoScale(point: Mach1Point3D) {
         Mach1DecodePositionalCAPI_setDecoderAlgoScale(M1obj, point)
         /// Sets the decode object's scale in desired x,y,z space
@@ -117,25 +164,41 @@ public class Mach1DecodePositional {
     public func getVolumesWalls(result: inout [Float]) {
         let pointer: UnsafeMutablePointer< Float > = UnsafeMutablePointer(&result)
         Mach1DecodePositionalCAPI_getVolumesWalls(M1obj, pointer);
+        /// Return the current coefficients to be applied to the audiopla yer's volume
     }
+
     public func getVolumesRoom(result: inout [Float]) {
         let pointer: UnsafeMutablePointer< Float > = UnsafeMutablePointer(&result)
         Mach1DecodePositionalCAPI_getVolumesRoom(M1obj, pointer);
+        /// Warning: Experimental feature
+        /// Remark: Part of BlendMode
     }
+
     public func getDist() -> Float {
         return Mach1DecodePositionalCAPI_getDist(M1obj)
+        /// Return a distance calculation of the position of the device/camera
+        /// to the position of the m1obj
     }
     
     public func getCurrentAngle() -> Mach1Point3D {
         return Mach1DecodePositionalCAPI_getCurrentAngle(M1obj)
+        /// Return the current orientation angle of the m1obj
     }
     
     public func getVolumeRotation() -> Mach1Point3D {
         return Mach1DecodePositionalCAPI_getVolumeRotation(M1obj)
+        /// Return the current pivot rotation due to the m1obj comparison to the
+        /// device/camera
     }
     
     public func setFilterSpeed(filterSpeed: Float) {
         Mach1DecodePositionalCAPI_setFilterSpeed(M1obj, filterSpeed)
+        /// Filter speed determines the amount of angle smoothing applied 
+        /// to the orientation angles used for the Mach1DecodeCore class
+        ///
+        /// - Parameters: 
+        ///     - value range: 0.0001 -> 1.0 (where 0.1 would be a slow filter
+        ///     and 1.0 is no filter)
     }
     
 }

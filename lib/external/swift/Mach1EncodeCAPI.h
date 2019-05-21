aa16932a8e5e3fa6edbf03da448f4d118f62e515
Dylan Marcus <dylan@mach1studios.com>
2019-05-21 13:05:55 -0400

    lib: bugfix for CAPI encode's new function

--- lib/external/swift/Mach1EncodeCAPI.h
+++ lib/external/swift/Mach1EncodeCAPI.h
@@ -47,7 +47,7 @@ extern "C" {
 	M1_API void* Mach1EncodeCAPI_getGains(void* M1obj);
 	M1_API void* Mach1EncodeCAPI_getPointsNames(void* M1obj);
 	M1_API void* Mach1EncodeCAPI_getGainsForInputChannelNamed(void* M1obj, char* pointName);
-	M1_API void* Mach1EncodeCAPI_getResultingVolumesDecoded(void* M1obj);
+	M1_API void* Mach1EncodeCAPI_getResultingVolumesDecoded(void* M1obj, enum Mach1DecodeAlgoType decodeType, float* decodeResult);
 
 	M1_API void Mach1EncodeCAPI_generatePointResults(void* M1obj);
 	M1_API int Mach1EncodeCAPI_getPointsCount(void* M1obj);

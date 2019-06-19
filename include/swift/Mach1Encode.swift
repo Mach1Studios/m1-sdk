9fe9b4bd3aaa5bcfe90f23a1c2ffc3321cabe3c6
andorxornot <andorxornot@gmail.com>
2019-06-19 18:08:14 +0300

    swift. update get gains

--- include/swift/Mach1Encode.swift
+++ include/swift/Mach1Encode.swift
@@ -69,24 +69,18 @@ public class Mach1Encode {
         var array = Array(repeating: String(), count: Int(Mach1EncodeCAPI_getPointsCount(M1obj)))
         let pointsPtr = unsafeBitCast( Mach1EncodeCAPI_getPointsNames(M1obj), to: UnsafeMutablePointer<UnsafeMutablePointer<Int8>?>?.self)
         for i in 0..<array.count {
-            if((pointsPtr! + i).pointee != nil ) {
-                array[i] = String(cString: (pointsPtr! + i).pointee!) // String.cString((pointsPtr! + i).pointee)
-            }
+            array[i] = String(cString: (pointsPtr! + i).pointee!) // String.cString((pointsPtr! + i).pointee)
         }
         return array
         /// Return the string name for each input channel/point
     }
 
     public func getGainsForInputChannelNamed(pointName: String) -> [Float] {
-        var array: [Float] = Array(repeating: 0.0, count: 7)
-        /*
+        var array: [Float] = Array(repeating: 0.0, count: 8)
         let gainsPtr = unsafeBitCast( Mach1EncodeCAPI_getGainsForInputChannelNamed(M1obj,  UnsafeMutablePointer<Int8>(mutating: (pointName as NSString).utf8String)), to: UnsafeMutablePointer<Float>?.self)
-        if(gainsPtr != nil) {
-            for i in 0..<array.count {
-                array[i] = (gainsPtr! + i).pointee
-            }
+        for i in 0..<array.count {
+            array[i] = (gainsPtr! + i).pointee
         }
-        */
         return array
         /// Returns array of gain coefficients for specificed input channel/point
     }

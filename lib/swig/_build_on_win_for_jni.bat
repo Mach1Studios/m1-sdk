3d67b1a46d5879686602274762e9632c8c86a834
andorxornot <andorxornot@gmail.com>
2019-06-25 22:17:33 +0300

    swig. updated dir structure for jni

--- lib/swig/_build_on_win_for_jni.bat
+++ lib/swig/_build_on_win_for_jni.bat
@@ -1,9 +1,10 @@
-mkdir jni
-D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni -I..\ -o jni\FloatArrayModule_wrap.cxx -package com.Mach1 FloatArrayModule.i 
-D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni -I..\ -o jni\Mach1Point3DModule_wrap.cxx -package com.Mach1 Mach1Point3DModule.i 
-D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni -I..\ -o jni\Mach1Point4DModule_wrap.cxx -package com.Mach1 Mach1Point4DModule.i 
-D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni -I..\ -o jni\Mach1DecodeModule_wrap.cxx -package com.Mach1 Mach1DecodeModule.i 
-D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni -I..\ -o jni\Mach1EncodeModule_wrap.cxx -package com.Mach1 Mach1EncodeModule.i 
-D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni -I..\ -o jni\Mach1DecodePositionalModule_wrap.cxx -package com.Mach1 Mach1DecodePositionalModule.i 
-del /Q jni\*Module.java
+mkdir jni\java\com\Mach1
+mkdir jni\cpp\Mach1
+D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni\java\com\Mach1 -I..\ -o jni\cpp\Mach1\FloatArrayModule_wrap.cxx -package com.Mach1 FloatArrayModule.i 
+D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni\java\com\Mach1 -I..\ -o jni\cpp\Mach1\Mach1Point3DModule_wrap.cxx -package com.Mach1 Mach1Point3DModule.i 
+D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni\java\com\Mach1 -I..\ -o jni\cpp\Mach1\Mach1Point4DModule_wrap.cxx -package com.Mach1 Mach1Point4DModule.i 
+D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni\java\com\Mach1 -I..\ -o jni\cpp\Mach1\Mach1DecodeModule_wrap.cxx -package com.Mach1 Mach1DecodeModule.i 
+D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni\java\com\Mach1 -I..\ -o jni\cpp\Mach1\Mach1EncodeModule_wrap.cxx -package com.Mach1 Mach1EncodeModule.i 
+D:\swigwin-4.0.0\swig.exe -c++ -java -outdir jni\java\com\Mach1 -I..\ -o jni\cpp\Mach1\Mach1DecodePositionalModule_wrap.cxx -package com.Mach1 Mach1DecodePositionalModule.i 
+del /Q jni\java\com\Mach1\*Module.java
 pause

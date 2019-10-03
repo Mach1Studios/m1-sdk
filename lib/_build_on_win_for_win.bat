c185cd515c0a8d71c11e5f3ea6326e99c2e574d6
Mach1Studios <dylan@mach1studios.com>
2019-10-03 16:28:55 -0400

    lib: updated bins for win, updated build bat

--- lib/_build_on_win_for_win.bat
+++ lib/_build_on_win_for_win.bat
@@ -1,24 +1,26 @@
-python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-14-2015
-python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-14-2015-win64
 python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-15-2017
 python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-15-2017-win64
 
 del /f /s /q _win 1>nul
 rmdir /s /q _win
 
-mkdir _win
-cd _win
-
-mkdir -p vs14\x86
-mkdir -p vs14\x64
-mkdir -p vs15\x86
-mkdir -p vs15\x64
-
 cd %~dp0
 
-xcopy _install\vs-14-2015\lib _win\vs14\x86
-xcopy _install\vs-14-2015-win64\lib _win\vs14\x64
-xcopy _install\vs-15-2017\lib _win\vs15\x86
-xcopy _install\vs-15-2017-win64\lib _win\vs15\x64
+xcopy _install\vs-15-2017\lib ..\binaries\windows-x86\x86 /Y
+xcopy _install\vs-15-2017-win64\lib ..\binaries\windows-x86_64\x64 /Y
+
+xcopy _install\vs-15-2017\libUnity ..\examples\mach1spatial-c\Unity\M1UnityDecodeTest\Assets\Mach1\Plugins\Windows\x86 /Y
+xcopy _install\vs-15-2017-win64\libUnity ..\examples\mach1spatial-c\Unity\M1UnityDecodeTest\Assets\Mach1\Plugins\Windows\x86_64 /Y
+
+xcopy _install\vs-15-2017\libUnity ..\examples\mach1spatial-c\Unity\DSPGraph\Assets\Mach1\Plugins\Windows\x86 /Y
+xcopy _install\vs-15-2017-win64\libUnity ..\examples\mach1spatial-c\Unity\DSPGraph\Assets\Mach1\Plugins\Windows\x86_64 /Y
+
+xcopy _install\vs-15-2017\lib '..\examples\mach1spatial-c\Unreal Engine\Mach1DecodePlugin\Source\Mach1DecodePlugin\ThirdParty\Mach1\bin\Win32' /Y
+xcopy _install\vs-15-2017-win64\lib '..\examples\mach1spatial-c\Unreal Engine\Mach1DecodePlugin\Source\Mach1DecodePlugin\ThirdParty\Mach1\bin\Win64' /Y
+xcopy _install\vs-15-2017\libUnity '..\examples\mach1spatial-c\Unreal Engine\Mach1DecodePlugin\Source\Mach1DecodePlugin\ThirdParty\Mach1\bin\Win32' /Y
+xcopy _install\vs-15-2017-win64\libUnity '..\examples\mach1spatial-c\Unreal Engine\Mach1DecodePlugin\Source\Mach1DecodePlugin\ThirdParty\Mach1\bin\Win64' /Y
+
+xcopy _install\vs-15-2017\lib ..\examples\mach1spatial-c\openframeworks\ofxMach1\libs\libmach1\lib\vs\x86 /Y
+xcopy _install\vs-15-2017-win64\lib ..\examples\mach1spatial-c\openframeworks\ofxMach1\libs\libmach1\lib\vs\x64 /Y
 
 pause

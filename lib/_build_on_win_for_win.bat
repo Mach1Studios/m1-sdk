0ccdef7f67c10f6ef097b580c732df504e42dd79
Mach1Studios <dylan@mach1studios.com>
2019-10-25 12:53:01 -0400

    libs: rebuilt for win targets and examples

--- lib/_build_on_win_for_win.bat
+++ lib/_build_on_win_for_win.bat
@@ -15,8 +15,8 @@ xcopy _install\vs-15-2017-win64\libUnity ..\examples\mach1spatial-c\Unity\M1Unit
 xcopy _install\vs-15-2017\libUnity ..\examples\mach1spatial-c\Unity\DSPGraph\Assets\Mach1\Plugins\Windows\x86 /Y
 xcopy _install\vs-15-2017-win64\libUnity ..\examples\mach1spatial-c\Unity\DSPGraph\Assets\Mach1\Plugins\Windows\x86_64 /Y
 
-xcopy _install\vs-15-2017\libUnity "..\examples\mach1spatial-c\Unreal Engine\Mach1DecodePlugin\Source\Mach1DecodePlugin\ThirdParty\Mach1\bin\Win32" /Y
-xcopy _install\vs-15-2017-win64\libUnity "..\examples\mach1spatial-c\Unreal Engine\Mach1DecodePlugin\Source\Mach1DecodePlugin\ThirdParty\Mach1\bin\Win64" /Y
+xcopy _install\vs-15-2017\libUnity "..\examples\mach1spatial-c\Unreal Engine\Mach1DecodePlugin\SourcePlugin\Mach1DecodePlugin\ThirdParty\Mach1\bin\Win32" /Y
+xcopy _install\vs-15-2017-win64\libUnity "..\examples\mach1spatial-c\Unreal Engine\Mach1DecodePlugin\SourcePlugin\Mach1DecodePlugin\ThirdParty\Mach1\bin\Win64" /Y
 
 xcopy _install\vs-15-2017\lib ..\examples\mach1spatial-c\openframeworks\ofxMach1\libs\libmach1\lib\vs\x86 /Y
 xcopy _install\vs-15-2017-win64\lib ..\examples\mach1spatial-c\openframeworks\ofxMach1\libs\libmach1\lib\vs\x64 /Y

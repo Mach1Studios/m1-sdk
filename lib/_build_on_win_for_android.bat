61f59f4d038a2ec2b6bfbe15b5756efd84545509
andorxornot <andorxornot@gmail.com>
2020-02-12 22:37:17 +0300

    fixed win script

--- lib/_build_on_win_for_android.bat
+++ lib/_build_on_win_for_android.bat
@@ -5,7 +5,7 @@ set CMAKE_HOME=%ANDROID_HOME%/cmake/3.6.4111459/bin
 set PATH=%PATH%;%CMAKE_HOME%;%ANDROID_HOME%;%ANDROID_HOME%/platform-tools;%ANDROID_HOME%/tools;%ANDROID_NDK%;%ANDROID_NDK%/prebuilt/windows-x86_64/bin;
 
 python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx
-python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-armeabi-v7a-clang-libcxx
+python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx
 python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-x86-clang-libcxx
 python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-x86-64-clang-libcxx
 
@@ -26,7 +26,7 @@ mkdir x86_64
 cd %~dp0
 
 xcopy _install\android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx\lib _android\arm64-v8a
-xcopy _install\android-ndk-r16b-api-21-armeabi-v7a-clang-libcxx\lib _android\armeabi-v7a
+xcopy _install\android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx\lib _android\armeabi-v7a
 xcopy _install\android-ndk-r16b-api-21-x86-clang-libcxx\lib _android\x86
 xcopy _install\android-ndk-r16b-api-21-x86-64-clang-libcxx\lib _android\x86_64
 pause

6d1f37f1e647f4e4e49598a1935d4bd861f03ff3
andorxornot <andorxornot@gmail.com>
2020-03-10 22:18:37 +0300

    commandline example. fix project and timer

--- examples/mach1spatial-c/commandline/spatial-encode-example/spatial-encode-example/main.cpp
+++ examples/mach1spatial-c/commandline/spatial-encode-example/spatial-encode-example/main.cpp
@@ -103,7 +103,7 @@ int main(int argc, const char * argv[]) {
     // time increment for Yaw/Pitch/Roll updates to decode
     struct timespec ts;
     ts.tv_sec =  0;
-    ts.tv_nsec = (long)1e7; // 1/100 seconds
+    ts.tv_nsec = (long)1e2;
     
     printf("Setting up\n");
     inputMode = Mach1EncodeInputModeMono;

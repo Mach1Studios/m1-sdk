be9bb9df90e201587bc51afefb31016af510559f
Mach1Studios <dylan@mach1studios.com>
2020-04-13 18:29:55 -0400

    lib: fixed build paths for win, built win libs and populated examples

--- lib/_build_on_win_for_android.bat
+++ lib/_build_on_win_for_android.bat
@@ -4,10 +4,10 @@ set ANDROID_HOME=D:\SDKs\android
 set CMAKE_HOME=%ANDROID_HOME%/cmake/3.6.4111459/bin
 set PATH=%PATH%;%CMAKE_HOME%;%ANDROID_HOME%;%ANDROID_HOME%/platform-tools;%ANDROID_HOME%/tools;%ANDROID_NDK%;%ANDROID_NDK%/prebuilt/windows-x86_64/bin;
 
-python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx
-python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx
-python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-x86-clang-libcxx
-python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-x86-64-clang-libcxx
+python ../../polly/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx
+python ../../polly/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx
+python ../../polly/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-x86-clang-libcxx
+python ../../polly/bin/polly.py --clear --install --config Release --toolchain android-ndk-r16b-api-21-x86-64-clang-libcxx
 
 del /f /s /q _android 1>nul
 rmdir /s /q _android
--- lib/_build_on_win_for_win.bat
+++ lib/_build_on_win_for_win.bat
@@ -1,5 +1,5 @@
-python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-15-2017
-python ../../polly-master/bin/polly.py --clear --install --config Release --toolchain vs-15-2017-win64
+python ../../polly/bin/polly.py --clear --install --config Release --toolchain vs-15-2017
+python ../../polly/bin/polly.py --clear --install --config Release --toolchain vs-15-2017-win64
 
 cd %~dp0
 
@@ -9,8 +9,8 @@ xcopy _install\vs-15-2017-win64\lib ..\binaries\windows-x86_64 /Y
 xcopy _install\vs-15-2017\libUnity ..\examples\mach1spatial-c\Unity\M1UnityDecodeTest\Assets\Mach1\Plugins\Windows\x86 /Y
 xcopy _install\vs-15-2017-win64\libUnity ..\examples\mach1spatial-c\Unity\M1UnityDecodeTest\Assets\Mach1\Plugins\Windows\x86_64 /Y
 
-xcopy _install\vs-15-2017\libUnity ..\examples\mach1spatial-c\Unity\DSPGraph\Assets\Mach1\Plugins\Windows\x86 /Y
-xcopy _install\vs-15-2017-win64\libUnity ..\examples\mach1spatial-c\Unity\DSPGraph\Assets\Mach1\Plugins\Windows\x86_64 /Y
+xcopy _install\vs-15-2017\libUnity ..\examples\mach1spatial-c\Unity\M1DSPGraph\Assets\Mach1\Plugins\Windows\x86 /Y
+xcopy _install\vs-15-2017-win64\libUnity ..\examples\mach1spatial-c\Unity\M1DSPGraph\Assets\Mach1\Plugins\Windows\x86_64 /Y
 
 xcopy _install\vs-15-2017\libUnity "..\examples\mach1spatial-c\Unreal Engine\Mach1DecodePlugin\SourcePlugin\Mach1DecodePlugin\ThirdParty\Mach1\bin\Win32" /Y
 xcopy _install\vs-15-2017-win64\libUnity "..\examples\mach1spatial-c\Unreal Engine\Mach1DecodePlugin\SourcePlugin\Mach1DecodePlugin\ThirdParty\Mach1\bin\Win64" /Y

ce17437ebd2c32ca7e2c1ebebd6eec2bd99e60e6
Dylan Marcus <dylan@mach1studios.com>
2020-06-03 16:16:57 -0400

    M1DSP: added 1Euro implementation

--- lib/include/M1DSP/M1DSPFilters.cpp
+++ lib/include/M1DSP/M1DSPFilters.cpp
@@ -23,7 +23,7 @@ using namespace M1DSP::Filters;
  - Make single global SetSampleRate(sr)
  */
 
-void CFilterSimpleLP::Setup(int cutoff_frequency, float resonance)
+void CFilterSimpleLP::Setup(int cutoff_frequency, double resonance)
 {
 	// https://www.musicdsp.org/en/latest/Filters/38-lp-and-hp-filter.html
 	this->cutoff_frequency = cutoff_frequency;
@@ -34,22 +34,22 @@ void CFilterSimpleLP::Setup(int cutoff_frequency, float resonance)
 	out2 = 0.0;
 }
 
-void CFilterSimpleLP::SetSampleRate(float fs)
+void CFilterSimpleLP::SetSampleRate(double fs)
 {
     this->sample_rate = fs;
 }
 
-float CFilterSimpleLP::Run(float in) {
+double CFilterSimpleLP::Run(double in)
+{
+	double c = 1.0 / tan(PI * cutoff_frequency / sample_rate);
 
-	float c = 1.0 / tan(PI * cutoff_frequency / sample_rate);
+	double a1 = 1.0 / (1.0 + resonance * c + c * c);
+	double a2 = 2 * a1;
+	double a3 = a1;
+	double b1 = 2.0 * (1.0 - c * c) * a1;
+	double b2 = (1.0 - resonance * c + c * c) * a1;
 
-	float a1 = 1.0 / (1.0 + resonance * c + c * c);
-	float a2 = 2 * a1;
-	float a3 = a1;
-	float b1 = 2.0 * (1.0 - c * c) * a1;
-	float b2 = (1.0 - resonance * c + c * c) * a1;
-
-	float out = a1 * in + a2 * in1 + a3 * in2 - b1 * out1 - b2 * out2;
+	double out = a1 * in + a2 * in1 + a3 * in2 - b1 * out1 - b2 * out2;
 
 	out2 = out1;
 	out1 = out;
@@ -75,9 +75,9 @@ CFilterButterworth24db::~CFilterButterworth24db(void)
 {
 }
 
-void CFilterButterworth24db::SetSampleRate(float fs)
+void CFilterButterworth24db::SetSampleRate(double fs)
 {
-    float pi = 4.f * atanf(1.f);
+    double pi = 4.f * atanf(1.f);
     
     this->t0 = 4.f * fs * fs;
     this->t1 = 8.f * fs * fs;
@@ -88,7 +88,7 @@ void CFilterButterworth24db::SetSampleRate(float fs)
     this->max_cutoff = fs * 0.45f;
 }
 
-void CFilterButterworth24db::Setup(float cutoff, float q)
+void CFilterButterworth24db::Setup(double cutoff, double q)
 {
     if (cutoff < this->min_cutoff)
         cutoff = this->min_cutoff;
@@ -100,8 +100,8 @@ void CFilterButterworth24db::Setup(float cutoff, float q)
     else if(q > 1.f)
         q = 1.f;
     
-    float wp = this->t2 * tanf(this->t3 * cutoff);
-    float bd, bd_tmp, b1, b2;
+    double wp = this->t2 * tanf(this->t3 * cutoff);
+    double bd, bd_tmp, b1, b2;
     
     q *= BUDDA_Q_SCALE;
     q += 1.f;
@@ -129,10 +129,10 @@ void CFilterButterworth24db::Setup(float cutoff, float q)
     this->coef3 = (bd_tmp - this->t2 * b1) * bd;
 }
 
-float CFilterButterworth24db::Run(float input)
+double CFilterButterworth24db::Run(double input)
 {
-    float output = input * this->gain;
-    float new_hist;
+    double output = input * this->gain;
+    double new_hist;
     
     output -= this->history1 * this->coef0;
     new_hist = output - this->history2 * this->coef1;
@@ -153,4 +153,96 @@ float CFilterButterworth24db::Run(float input)
     this->history3 = new_hist;
     
     return output;
+}
+
+void CFilterLowPass::setAlpha(double alpha)
+{
+    if (alpha<=0.0 || alpha>1.0)
+        throw std::range_error("alpha should be in (0.0., 1.0]") ;
+    a = alpha ;
+}
+
+CFilterLowPass::CFilterLowPass(double alpha, double initval) {
+    y = s = initval ;
+    setAlpha(alpha) ;
+    initialized = false ;
+}
+
+double CFilterLowPass::Run(double input) {
+    double result;
+    if (initialized)
+        result = a*input + (1.0-a)*s;
+    else {
+        result = input;
+        initialized = true;
+    }
+    y = input;
+    s = result;
+    return result;
+}
+
+double CFilterLowPass::filterWithAlpha(double value, double alpha) {
+    setAlpha(alpha);
+    return Run(value);
+}
+
+bool CFilterLowPass::hasLastRawValue(void) {
+    return initialized;
+}
+
+double CFilterLowPass::lastRawValue(void) {
+    return y;
+}
+
+void CFilterOneEuro::Setup(double mincutoff, double beta_, double dcutoff)
+{
+    // http://cristal.univ-lille.fr/~casiez/1euro/
+    setMinCutoff(mincutoff);
+    setBeta(beta_);
+    setDerivateCutoff(dcutoff);
+    x = new CFilterLowPass(alpha(mincutoff));
+    dx = new CFilterLowPass(alpha(dcutoff));
+}
+
+double CFilterOneEuro::alpha(double cutoff) {
+    double te = 1.0 / freq ;
+    double tau = 1.0 / (2*M_PI*cutoff) ;
+    return 1.0 / (1.0 + tau/te) ;
+}
+
+void CFilterOneEuro::setMinCutoff(double mc) {
+    if (mc<=0) throw std::range_error("mincutoff should be >0") ;
+    mincutoff = mc ;
+}
+
+void CFilterOneEuro::setBeta(double b) {
+    beta_ = b ;
+}
+
+void CFilterOneEuro::setDerivateCutoff(double dc) {
+    if (dc<=0) throw std::range_error("dcutoff should be >0") ;
+    dcutoff = dc ;
+}
+
+double CFilterOneEuro::Run(double input)
+{
+    // estimate the current variation per second
+    double dvalue = x->hasLastRawValue() ? (input - x->lastRawValue())*freq : 0.0 ; // FIXME: 0.0 or value?
+    double edvalue = dx->filterWithAlpha(dvalue, alpha(dcutoff)) ;
+    // use it to update the cutoff frequency
+    double cutoff = mincutoff + beta_*fabs(edvalue) ;
+    // filter the given value
+    return x->filterWithAlpha(input, alpha(cutoff)) ;
+}
+
+void CFilterOneEuro::SetSampleRate(double fs)
+{
+    if (fs<=0) throw std::range_error("freq should be >0");
+    freq = fs;
+}
+
+CFilterOneEuro::~CFilterOneEuro(void)
+{
+    delete x;
+    delete dx;
 }
--- lib/include/M1DSP/M1DSPFilters.h
+++ lib/include/M1DSP/M1DSPFilters.h
@@ -12,19 +12,19 @@ namespace M1DSP
 	{
 		class CFilterSimpleLP
         {
+        public:
+            void Setup(int cutoff_frequency, double resonance);
+            void SetSampleRate(double fs); //TODO: move to global SetSampleRate(double) function
+            double Run(double in);
+            
         private:
 			int sample_rate;
 			int cutoff_frequency;
-			float resonance;
-			float in1;
-			float in2;
-			float out1;
-			float out2;
-
-		public:
-			void Setup(int cutoff_frequency, float resonance);
-            void SetSampleRate(float fs); //TODO: move to global SetSampleRate(float) function
-			float Run(float in);
+			double resonance;
+			double in1;
+			double in2;
+			double out1;
+			double out2;
 		};
         
         class CFilterButterworth24db
@@ -32,16 +32,55 @@ namespace M1DSP
         public:
             CFilterButterworth24db(void);
             ~CFilterButterworth24db(void);
-            void SetSampleRate(float fs); //TODO: move to global SetSampleRate(float) function
-            void Setup(float cutoff, float q);
-            float Run(float input);
+            void SetSampleRate(double fs); //TODO: move to global SetSampleRate(double) function
+            void Setup(double cutoff, double q);
+            double Run(double input);
             
         private:
-            float t0, t1, t2, t3;
-            float coef0, coef1, coef2, coef3;
-            float history1, history2, history3, history4;
-            float gain;
-            float min_cutoff, max_cutoff;
+            double t0, t1, t2, t3;
+            double coef0, coef1, coef2, coef3;
+            double history1, history2, history3, history4;
+            double gain;
+            double min_cutoff, max_cutoff;
+        };
+        
+        class CFilterLowPass
+        {
+        private:
+            double y, a, s;
+            bool initialized;
+            
+            void setAlpha(double alpha);
+            
+        public:
+            CFilterLowPass(double alpha, double initval=0.0);
+            double Run(double input);
+            double filterWithAlpha(double value, double alpha);
+            bool hasLastRawValue(void);
+            double lastRawValue(void);
+        };
+        
+        class CFilterOneEuro
+        {
+        public:
+            CFilterOneEuro(void);
+            ~CFilterOneEuro(void);
+            void Setup(double mincutoff=1.0, double beta_=0.0, double dcutoff=1.0);
+            void SetSampleRate(double fs); //TODO: move to global SetSampleRate(double) function
+            double Run(double input);
+            
+        private:
+            double freq;
+            double mincutoff;
+            double beta_;
+            double dcutoff;
+            CFilterLowPass *x;
+            CFilterLowPass *dx;
+            
+            double alpha(double cutoff);
+            void setMinCutoff(double mc);
+            void setBeta(double b);
+            void setDerivateCutoff(double dc);
         };
 	}
 }

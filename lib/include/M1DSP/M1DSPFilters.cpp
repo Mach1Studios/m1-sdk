81f4ba04374abb8040c418867629c5cdf4ae1f10
Mach1Studios <dylan@mach1studios.com>
2020-06-09 17:29:44 -0400

    sdk: bug fix for windows declarations

--- lib/include/M1DSP/M1DSPFilters.cpp
+++ lib/include/M1DSP/M1DSPFilters.cpp
@@ -7,7 +7,7 @@ using namespace M1DSP::Filters;
 
 #ifndef PI
 #define PI       3.14159265358979323846
-#endif 
+#endif
 
 #define BUDDA_Q_SCALE 6.f
 
@@ -15,10 +15,10 @@ using namespace M1DSP::Filters;
  Filters should utilize the following structure for ease of use
  - Setup(cutoff, q)
  - Run(input)
- 
+
  cutoff: 0hz -> nyquist/2 hz
  q: 0.0 (no q) -> 1.0 (full q)
- 
+
  TODO:
  - Make single global SetSampleRate(sr)
  */
@@ -66,7 +66,7 @@ CFilterButterworth24db::CFilterButterworth24db(void)
     this->history2 = 0.f;
     this->history3 = 0.f;
     this->history4 = 0.f;
-    
+
     this->SetSampleRate(44100.f);
     this->Setup(22050.f, 0.0);
 }
@@ -78,12 +78,12 @@ CFilterButterworth24db::~CFilterButterworth24db(void)
 void CFilterButterworth24db::SetSampleRate(double fs)
 {
     double pi = 4.f * atanf(1.f);
-    
+
     this->t0 = 4.f * fs * fs;
     this->t1 = 8.f * fs * fs;
     this->t2 = 2.f * fs;
     this->t3 = pi / fs;
-    
+
     this->min_cutoff = fs * 0.01f;
     this->max_cutoff = fs * 0.45f;
 }
@@ -94,36 +94,36 @@ void CFilterButterworth24db::Setup(double cutoff, double q)
         cutoff = this->min_cutoff;
     else if(cutoff > this->max_cutoff)
         cutoff = this->max_cutoff;
-    
+
     if(q < 0.f)
         q = 0.f;
     else if(q > 1.f)
         q = 1.f;
-    
+
     double wp = this->t2 * tanf(this->t3 * cutoff);
     double bd, bd_tmp, b1, b2;
-    
+
     q *= BUDDA_Q_SCALE;
     q += 1.f;
-    
+
     b1 = (1.847759f / (q * wp));
     b2 = 1.f / (wp * wp);
-    
+
     bd_tmp = this->t0 * b2 + 1.f;
-    
+
     bd = 1.f / (bd_tmp + this->t2 * b1);
-    
+
     this->gain = bd;
-    
+
     this->coef2 = (2.f - this->t1 * b2);
-    
+
     this->coef0 = this->coef2 * bd;
     this->coef1 = (bd_tmp - this->t2 * b1) * bd;
-    
+
     b1 = (1.847759f / q) / wp;
-    
+
     bd = 1.f / (bd_tmp + this->t2 * b1);
-    
+
     this->gain *= bd;
     this->coef2 *= bd;
     this->coef3 = (bd_tmp - this->t2 * b1) * bd;
@@ -133,25 +133,25 @@ double CFilterButterworth24db::Run(double input)
 {
     double output = input * this->gain;
     double new_hist;
-    
+
     output -= this->history1 * this->coef0;
     new_hist = output - this->history2 * this->coef1;
-    
+
     output = new_hist + this->history1 * 2.f;
     output += this->history2;
-    
+
     this->history2 = this->history1;
     this->history1 = new_hist;
-    
+
     output -= this->history3 * this->coef2;
     new_hist = output - this->history4 * this->coef3;
-    
+
     output = new_hist + this->history3 * 2.f;
     output += this->history4;
-    
+
     this->history4 = this->history3;
     this->history3 = new_hist;
-    
+
     return output;
 }
 
@@ -206,7 +206,7 @@ void CFilterOneEuro::Setup(double mincutoff, double beta_, double dcutoff)
 
 double CFilterOneEuro::alpha(double cutoff) {
     double te = 1.0 / freq ;
-    double tau = 1.0 / (2*M_PI*cutoff) ;
+    double tau = 1.0 / (2*PI*cutoff) ;
     return 1.0 / (1.0 + tau/te) ;
 }
 

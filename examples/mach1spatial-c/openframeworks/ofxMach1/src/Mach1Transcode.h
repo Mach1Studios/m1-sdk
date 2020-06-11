19faf09a744996d013451ee62ac1ab5c985f6746
Dylan Marcus <dylan@mach1studios.com>
2020-06-11 16:20:07 -0400

    libs: added built transcode libs to all examples

---
+++ examples/mach1spatial-c/openframeworks/ofxMach1/src/Mach1Transcode.h
@@ -0,0 +1,46 @@
+//  Mach1 Spatial SDK
+//  Copyright © 2017-2020 Mach1. All rights reserved.
+
+#pragma once
+
+#include <vector>
+#include <string>
+#include "Mach1TranscodeCAPI.h"
+
+class Mach1Transcode {
+private:
+	void* M1obj;
+	
+public:
+	Mach1Transcode();
+	~Mach1Transcode();
+
+	int getInputNumChannels();
+	int getOutputNumChannels();
+	Mach1TranscodeFormatType getFormatFromString(char* str);
+	char* getFormatName(Mach1TranscodeFormatType fmt);
+
+	float calcNormalization(float** bufs, int numSamples);
+	void applyMasterGain(float** bufs, int numSamples, float masterGain = 1.0f);
+
+	float db2level(float db);
+	float level2db(float level);
+
+	void setLFESub(std::vector<int> subChannelIndices, int sampleRate);
+	void setSpatialDownmixer(float corrThreshold = 0.1);
+	bool getSpatialDownmixerPossibility();
+
+	void setInputFormat(Mach1TranscodeFormatType inFmt);
+	void setInputFormatADM(char* inXml);
+	void setInputFormatTTJson(char* inJson);
+	void setInputFormatTTPoints(std::vector<Mach1Point3D> points);
+
+	void setOutputFormat(Mach1TranscodeFormatType outFmt);
+	void setOutputFormatTTJson(char* outJson);
+	void setOutputFormatTTPoints(std::vector<Mach1Point3D> points);
+
+	bool computeConvertionPath();
+	void convert(float** inBufs, float** outBufs, int numSamples);
+
+	std::vector<Mach1TranscodeFormatType> getFormatsConvertionPath();
+};

5153250ce990c3a70ad6c9a8aa6ba12937606b55
Dylan Marcus <dylan@mach1studios.com>
2020-06-12 17:22:02 -0400

    web-example: fixed download script

--- examples/mach1spatial-web/download-audiofiles.sh
+++ examples/mach1spatial-web/download-audiofiles.sh
@@ -36,10 +36,10 @@ wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T1.ogg
 wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T2.ogg
 wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T3.ogg
 wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T4.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B1.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B2.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B3.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B4.ogg
+wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B5.ogg
+wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B6.ogg
+wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B7.ogg
+wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B8.ogg
 
 cd ../../video
 wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/video/video.webm

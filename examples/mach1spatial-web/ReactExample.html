a1a93e0ca393ae96bceafe4ca584676332de9c88
andorxornot <andorxornot@gmail.com>
2020-07-08 19:33:16 +0300

    examples. added react component example

---
+++ examples/mach1spatial-web/ReactExample.html
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Add React in One Minute</title>
+  </head>
+  <body>
+
+    <h2>Add React in One Minute</h2>
+    <p>This page demonstrates using React with no build tooling.</p>
+    <p>React is loaded as a script tag.</p>
+
+    <!-- We will put our React component inside this div. -->
+    <div id="like_button_container"></div>
+
+    <!-- Load React. -->
+    <!-- Note: when deploying, replace "development.js" with "production.min.js". -->
+    <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+
+    <!-- Load our React component. -->
+    <script src="like_button.js"></script>
+
+  </body>
+</html>
---
+++ examples/mach1spatial-web/js/M1DecodeReactComponent.js
@@ -0,0 +1,52 @@
+'use strict';
+
+class M1DecodeReactComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            decoded: Array(),
+            FRAMES_PER_SECOND: 60,
+            m1Decode: null,
+        };
+
+        Mach1DecodeModule().then(function(m1DecodeModule) {
+            var m1Decode = new(m1DecodeModule).Mach1Decode();
+            m1Decode.setPlatformType(m1Decode.Mach1PlatformType.Mach1PlatformOfEasyCam);
+            m1Decode.setDecodeAlgoType(m1Decode.Mach1DecodeAlgoType.Mach1DecodeAlgoSpatial);
+            m1Decode.setFilterSpeed(0.95);
+
+            this.state.m1Decode = m1Decode;
+        }.bind(this));
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000 / this.state.FRAMES_PER_SECOND
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+
+    tick() {
+        console.log('tick!');
+
+        // check decode for loading
+        if (this.state.m1Decode) {
+            this.state.m1Decode.beginBuffer();
+            let decoded = this.state.m1Decode.decode(0, 0, 0);
+            this.state.m1Decode.endBuffer();
+
+            this.setState({
+                decoded: decoded
+            });
+        }
+    }
+
+    render() {
+        return React.createElement('div', null, this.state.decoded.join(' '));
+    }
+}

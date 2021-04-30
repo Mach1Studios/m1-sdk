7f1b44e28bf3a608a1daad6f7711638e83585af9
Dylan Marcus <dylan@mach1studios.com>
2021-04-30 00:20:21 -0400

    tests: more improvements to testing

--- lib/tests/_build_on_linux_for_linux.sh
+++ lib/tests/_build_on_linux_for_linux.sh
@@ -1,6 +1,6 @@
 echo "### BUILD Linux ###"
 echo "### REMINDER: Set alias for polly ###"
-polly --clear --install --config Release --toolchain gcc
+~/git/polly/bin/polly --clear --install --config Release --toolchain gcc
 
 echo "### RUN TESTS ###"
 _install/gcc/bin/Mach1EncodeTest
@@ -8,7 +8,7 @@ _install/gcc/bin/Mach1DecodeTest
 _install/gcc/bin/Mach1DecodePositionalTest
 
 echo "### RUN LINKING TESTS ON CURRENT DEVICE ###"
-cd ../ && polly --clear --install --config Release --toolchain gcc
+cd ../ && ~/git/polly/bin/polly --clear --install --config Release --toolchain gcc
 g++ -o spatial-decode-exmple -I../../include/cpp -L../_install/gcc/lib -lMach1DecodeCAPI ../../examples/mach1spatial-c/commandline/spatial-decode-example/main.cpp 
 g++ -o spatial-encode-exmple -I../../include/cpp -L../_install/gcc/lib -lMach1EncodeCAPI ../../examples/mach1spatial-c/commandline/spatial-encode-example/main.cpp 
 g++ -o spatial-decodepositional-exmple -I../../include/cpp -L../_install/gcc/lib -lMach1DecodeCAPI -lMach1DecodePositionalCAPI ../../examples/mach1spatial-c/commandline/spatial-decodepositional-example/main.cpp 

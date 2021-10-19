a0d031c3bf2006ea7a7636f4c21ca5763c0df834
Dylan Marcus <dylan@mach1studios.com>
2021-10-19 11:21:30 -0400

    tests: fixed testing for linux targets

--- lib/tests/_build_on_linux_for_linux.sh
+++ lib/tests/_build_on_linux_for_linux.sh
@@ -9,7 +9,7 @@ _install/gcc/bin/Mach1DecodePositionalTest
 
 echo "### RUN LINKING TESTS ON CURRENT DEVICE ###"
 cd ../ && ~/git/polly/bin/polly --clear --install --config Release --toolchain gcc
-g++ -o spatial-decode-exmple -I../../include/cpp -L../_install/gcc/lib -lMach1DecodeCAPI ../../examples/mach1spatial-c/commandline/spatial-decode-example/main.cpp 
-g++ -o spatial-encode-exmple -I../../include/cpp -L../_install/gcc/lib -lMach1EncodeCAPI ../../examples/mach1spatial-c/commandline/spatial-encode-example/main.cpp 
-g++ -o spatial-decodepositional-exmple -I../../include/cpp -L../_install/gcc/lib -lMach1DecodeCAPI -lMach1DecodePositionalCAPI ../../examples/mach1spatial-c/commandline/spatial-decodepositional-example/main.cpp 
-g++ -o spatial-transcode-exmple -I../../include/cpp -L../_install/gcc/lib -lMach1TranscodeCAPI ../../examples/mach1spatial-c/commandline/spatial-transcode-example/main.cpp 
+g++ -o spatial-decode-exmple -I../../include/cpp -L../_install/gcc/lib -lMach1DecodeCAPI ../../examples/mach1spatial-c/commandline/spatial-decode-example/spatial-decode-example/main.cpp 
+g++ -o spatial-encode-exmple -I../../include/cpp -L../_install/gcc/lib -lMach1EncodeCAPI ../../examples/mach1spatial-c/commandline/spatial-encode-example/spatial-encode-example/main.cpp 
+g++ -o spatial-decodepositional-exmple -I../../include/cpp -L../_install/gcc/lib -lMach1DecodeCAPI -lMach1DecodePositionalCAPI ../../examples/mach1spatial-c/commandline/spatial-decodepositional-example/spatial-decodepositional-example/main.cpp 
+g++ -o spatial-transcode-exmple -I../../include/cpp -L../_install/gcc/lib -lMach1TranscodeCAPI ../../examples/mach1spatial-c/commandline/spatial-transcode-example/spatial-transcode-example/main.cpp 

cfcb76372b5a075c3a49571c7b6e8e3028e5e689
Dylan Marcus <dylan@mach1studios.com>
2022-02-16 12:35:55 -0500

    source: added test to testing chain

--- source/tests/_build_on_linux_for_linux.sh
+++ source/tests/_build_on_linux_for_linux.sh
@@ -16,4 +16,5 @@ g++ -o spatial-transcode-exmple -I../include/cpp -L_install/gcc/lib -lpthread -l
 
 echo "### RENDER CHECK ####"
 echo "### 5.1_C -> M1Spatial ###"
+rm -rf Mach1TranscodeRenderTest/build
 cd Mach1TranscodeRenderTest && mkdir build && cd build && cmake ../ && cmake --build . && ./spatial-transcode-render -in-file "../m1-debug-shortpt-fiveone.wav" -in-fmt 5.1_C -out-file "../m1-debug-shortpt-fiveone_m1spatial-out.wav" -out-fmt M1Spatial -out-file-chans 0
--- source/tests/_build_on_osx_for_osx.sh
+++ source/tests/_build_on_osx_for_osx.sh
@@ -8,4 +8,5 @@ _install/xcode/bin/Mach1DecodePositionalTest
 
 echo "### RENDER CHECK ####"
 echo "### 5.1_C -> M1Spatial ###"
+rm -rf Mach1TranscodeRenderTest/build
 cd Mach1TranscodeRenderTest && mkdir build && cd build && cmake ../ && cmake --build . && ./spatial-transcode-render -in-file "../m1-debug-shortpt-fiveone.wav" -in-fmt 5.1_C -out-file "../m1-debug-shortpt-fiveone_m1spatial-out.wav" -out-fmt M1Spatial -out-file-chans 0

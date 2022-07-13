acab6cec00ffc789843ae09f2e51a673894ac2c1
Dylan Marcus <dylan@mach1studios.com>
2022-07-12 23:51:37 -0400

    source: changed two travis recipes to linux for faster processing, some windows fixes

--- source/scripts/win_test.sh
+++ source/scripts/win_test.sh
@@ -3,8 +3,8 @@
 cd ${TRAVIS_BUILD_DIR}/source/tests
 
 echo "WIN: TESTS"
-cmake . -B_builds/vs-15-2017 -G "Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=`pwd`/_install/vs-15-2017
-cmake --build _builds/vs-15-2017 --config Release --target install -- /nologo /verbosity:minimal
+cmake . -B_builds/vs-15-2017 -G "Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=_install/vs-15-2017
+cmake --build _builds/vs-15-2017 --config Release --target install -- //nologo //verbosity:quiet //clp:ErrorsOnly
 
 echo "RUN TESTS"
 echo " " | _install/vs-15-2017/bin/Mach1EncodeTests.exe

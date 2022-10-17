748c7a85e9e416f0404f0f7b236da8ff54bc28b8
Dylan Marcus <dylan@mach1studios.com>
2022-10-17 11:16:16 -0400

    ci/cd: test improvements

--- source/scripts/macos_setup.sh
+++ source/scripts/macos_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd ${TRAVIS_BUILD_DIR}
-echo "INSTALLING PYTHON3"
-wget --quiet https://www.python.org/ftp/python/3.8.4/Python-3.8.4.tgz
-tar -xf Python-3.8.4.tgz
-cd Python-3.8.4
-./configure 1> /dev/null
-make -s
-make -s install 
+# cd ${TRAVIS_BUILD_DIR}
+# echo "INSTALLING PYTHON3"
+# wget --quiet https://www.python.org/ftp/python/3.8.4/Python-3.8.4.tgz
+# tar -xf Python-3.8.4.tgz
+# cd Python-3.8.4
+# ./configure 1> /dev/null
+# make -s
+# make -s install 
 
 cd ${TRAVIS_BUILD_DIR}
 echo "INSTALLING SNDFILE"

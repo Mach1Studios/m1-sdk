534f44c63a261864b5ab9a655fb5677ba746dfdc
Dylan Marcus <dylan@mach1studios.com>
2022-10-17 12:04:00 -0400

    ci/cd: cleanup [ci skip]

--- source/scripts/macos_setup.sh
+++ source/scripts/macos_setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# cd ${TRAVIS_BUILD_DIR}
-# echo "INSTALLING PYTHON3"
-# wget --quiet https://www.python.org/ftp/python/3.8.4/Python-3.8.4.tgz
-# tar -xf Python-3.8.4.tgz
-# cd Python-3.8.4
-# ./configure 1> /dev/null
-# make -s
-# make -s install 
-
 cd ${TRAVIS_BUILD_DIR}
 echo "INSTALLING SNDFILE"
 wget --quiet https://github.com/libsndfile/libsndfile/releases/download/1.0.31/libsndfile-1.0.31.tar.bz2

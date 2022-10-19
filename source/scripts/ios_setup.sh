6f8bedfcc5f2638b3dc1008b6b524d42e4174a8b
Dylan Marcus <dylan@mach1studios.com>
2022-10-19 19:24:46 -0400

    ci/cd: skip processes if files are cached (ios)

--- source/scripts/ios_setup.sh
+++ source/scripts/ios_setup.sh
@@ -2,8 +2,8 @@
 
 cd ${TRAVIS_BUILD_DIR}
 echo "INSTALLING CMAKE"
-wget --quiet https://github.com/Kitware/CMake/releases/download/v3.19.3/cmake-3.19.3.zip
-unzip -qq cmake-3.19.3.zip
+wget --quiet -nc https://github.com/Kitware/CMake/releases/download/v3.19.3/cmake-3.19.3.zip
+unzip -qq -n cmake-3.19.3.zip
 cd cmake-3.19.3
 ./bootstrap 1> /dev/null
 make -s 

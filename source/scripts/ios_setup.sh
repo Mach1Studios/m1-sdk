b8b9843d6bbaa3c2b993a2c735e8c1dc0ef9a28c
Dylan Marcus <dylan@mach1studios.com>
2022-10-19 17:10:02 -0400

    ci/cd: more travis improvements

--- source/scripts/ios_setup.sh
+++ source/scripts/ios_setup.sh
@@ -9,12 +9,3 @@ cd cmake-3.19.3
 make -s 
 sudo make -s install
 export PATH=/usr/local/bin/cmake:$PATH
-
-cd ${TRAVIS_BUILD_DIR}
-echo "INSTALLING SNDFILE"
-wget --quiet https://github.com/libsndfile/libsndfile/releases/download/1.0.31/libsndfile-1.0.31.tar.bz2
-tar -xf libsndfile-1.0.31.tar.bz2
-cd libsndfile-1.0.31/
-./configure 1> /dev/null
-make -s
-make -s install

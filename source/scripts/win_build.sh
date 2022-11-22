59dcfdf7bcf3ce80b9cf2d17f491731c918ae849
Dylan Marcus <dylan@mach1.tech>
2022-11-22 11:32:57 -0500

    source: fixed some issues compiling windows shared libs

--- source/scripts/win_build.sh
+++ source/scripts/win_build.sh
@@ -2,36 +2,42 @@
 
 cd ${TRAVIS_BUILD_DIR}/source
 
+# dynamic win32
+cmake . -B_builds/vs-15-2017 -G "Visual Studio 15 2017" -DBUILD_SHARED_LIBS=1 -A Win32 -DCMAKE_INSTALL_PREFIX=_install/vs-15-2017
+cmake --build _builds/vs-15-2017 --config Release --target install -- //nologo //verbosity:quiet //clp:ErrorsOnly
+rsync -c "_install/vs-15-2017/lib-shared" "../mach1spatial-libs/vs-15-2017-x86/lib/Dynamic/Release/"
+aws s3 sync _install/vs-15-2017/lib-shared/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86/lib/Dynamic/Release --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
+rsync -c "_install/vs-15-2017/lib-shared" "../mach1spatial-libs/vs-15-2017-x86/lib/Dynamic/Debug/"
+aws s3 sync _install/vs-15-2017/lib-shared/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86/lib/Dynamic/Debug --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
+# dynamic x64
+cmake . -B_builds/vs-15-2017-win64 -G "Visual Studio 15 2017" -DBUILD_SHARED_LIBS=1 -A x64 -DCMAKE_INSTALL_PREFIX=_install/vs-15-2017-win64
+cmake --build _builds/vs-15-2017-win64 --config Release --target install -- //nologo //verbosity:quiet //clp:ErrorsOnly
+rsync -c "_install/vs-15-2017-win64/lib-shared" "../mach1spatial-libs/vs-15-2017-x86_64/lib/Dynamic/Release/"
+aws s3 sync _install/vs-15-2017-win64/lib-shared/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86_64/lib/Dynamic/Release --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
+rsync -c "_install/vs-15-2017-win64/lib-shared" "../mach1spatial-libs/vs-15-2017-x86_64/lib/Dynamic/Debug/"
+aws s3 sync _install/vs-15-2017-win64/lib-shared/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86_64/lib/Dynamic/Debug --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
 # MD win32
 cmake . -B_builds/vs-15-2017 -G "Visual Studio 15 2017" -A Win32 -DCMAKE_INSTALL_PREFIX=_install/vs-15-2017
 cmake --build _builds/vs-15-2017 --config Release --target install -- //nologo //verbosity:quiet //clp:ErrorsOnly
 rsync -c "_install/vs-15-2017/lib" "../mach1spatial-libs/vs-15-2017-x86/lib/Static/MD/Release/"
 aws s3 sync _install/vs-15-2017/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86/lib/Static/MD/Release --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
-rsync -c "_install/vs-15-2017/lib-shared" "../mach1spatial-libs/vs-15-2017-x86/lib/Dynamic/Release/"
-aws s3 sync _install/vs-15-2017/lib-shared/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86/lib/Dynamic/Release --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
 rsync -c "_install/vs-15-2017/lib" "../mach1spatial-libs/windows-x86"
 aws s3 sync _install/vs-15-2017/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/windows-x86 --cache-control no-cache --exclude "*-minifiedCAPI*" --metadata-directive REPLACE
 cmake . -B_builds/vs-15-2017 -G "Visual Studio 15 2017" -A Win32 -DCMAKE_INSTALL_PREFIX=_install/vs-15-2017
 cmake --build _builds/vs-15-2017 --config Debug --target install -- //nologo //verbosity:quiet //clp:ErrorsOnly
 rsync -c "_install/vs-15-2017/lib" "../mach1spatial-libs/vs-15-2017-x86/lib/Static/MD/Debug/"
 aws s3 sync _install/vs-15-2017/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86/lib/Static/MD/Debug --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
-rsync -c "_install/vs-15-2017/lib-shared" "../mach1spatial-libs/vs-15-2017-x86/lib/Dynamic/Debug/"
-aws s3 sync _install/vs-15-2017/lib-shared/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86/lib/Dynamic/Debug --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
 # MD x64
 cmake . -B_builds/vs-15-2017-win64 -G "Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=_install/vs-15-2017-win64
 cmake --build _builds/vs-15-2017-win64 --config Release --target install -- //nologo //verbosity:quiet //clp:ErrorsOnly
 rsync -c "_install/vs-15-2017-win64/lib" "../mach1spatial-libs/vs-15-2017-x86_64/lib/Static/MD/Release/"
 aws s3 sync _install/vs-15-2017-win64/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86_64/lib/Static/MD/Release --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
-rsync -c "_install/vs-15-2017-win64/lib-shared" "../mach1spatial-libs/vs-15-2017-x86_64/lib/Dynamic/Release/"
-aws s3 sync _install/vs-15-2017-win64/lib-shared/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86_64/lib/Dynamic/Release --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
 rsync -c "_install/vs-15-2017-win64/lib" "../mach1spatial-libs/windows-x86_64"
 aws s3 sync _install/vs-15-2017-win64/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/windows-x86_64 --cache-control no-cache --exclude "*-minifiedCAPI*" --metadata-directive REPLACE
 cmake . -B_builds/vs-15-2017-win64 -G "Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=_install/vs-15-2017-win64
 cmake --build _builds/vs-15-2017-win64 --config Debug --target install -- //nologo //verbosity:quiet //clp:ErrorsOnly
 rsync -c "_install/vs-15-2017-win64/lib" "../mach1spatial-libs/vs-15-2017-x86_64/lib/Static/MD/Debug/"
 aws s3 sync _install/vs-15-2017-win64/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86_64/lib/Static/MD/Debug --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
-rsync -c "_install/vs-15-2017-win64/lib-shared" "../mach1spatial-libs/vs-15-2017-x86_64/lib/Dynamic/Debug/"
-aws s3 sync _install/vs-15-2017-win64/lib-shared/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/vs-15-2017-x86_64/lib/Dynamic/Debug --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
 # MT win32
 cmake . -B_builds/vs-15-2017-mt -G "Visual Studio 15 2017" -A Win32 -DCOMPILE_MT=1 -DCMAKE_INSTALL_PREFIX=_install/vs-15-2017-mt
 cmake --build _builds/vs-15-2017-mt --config Release --target install -- //nologo //verbosity:quiet //clp:ErrorsOnly

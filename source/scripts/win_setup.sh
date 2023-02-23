3a6f7fc4d2744b7722f5f935b30774f0cb627ada
Dylan Marcus <dylan@mach1studios.com>
2023-02-22 19:58:32 -0500

    ci/cd: another shell fix for windows

--- source/scripts/win_setup.sh
+++ source/scripts/win_setup.sh
@@ -18,4 +18,19 @@ if [ ! -z "$PATH" ]; then
     mkdir -p ${TRAVIS_BUILD_DIR}/source/_install/vs-15-2017-win64-mt/Debug/lib
 else
     # Local non CI/CD install
+    if [[ "$PWD" == *source ]]
+    then
+        mkdir -p _install/vs-15-2017/Release/lib-shared
+        mkdir -p _install/vs-15-2017/Debug/lib-shared
+        mkdir -p _install/vs-15-2017-win64/Release/lib-shared
+        mkdir -p _install/vs-15-2017-win64/Debug/lib-shared
+        mkdir -p _install/vs-15-2017/Release/lib
+        mkdir -p _install/vs-15-2017/Debug/lib
+        mkdir -p _install/vs-15-2017-win64/Release/lib
+        mkdir -p _install/vs-15-2017-win64/Debug/lib
+        mkdir -p _install/vs-15-2017-mt/Release/lib
+        mkdir -p _install/vs-15-2017-mt/Debug/lib
+        mkdir -p _install/vs-15-2017-win64-mt/Release/lib
+        mkdir -p _install/vs-15-2017-win64-mt/Debug/lib
+    fi
 fi

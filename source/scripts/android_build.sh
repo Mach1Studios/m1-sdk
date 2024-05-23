98f20b03d91e7f273099887f938902473282ca72
Dylan Marcus <dylan@mach1studios.com>
2024-05-23 11:25:46 -0700

    Travis test (#18)
    * change android build process to include shared libs
    
    * upgraded version
    
    * resolve ssl issue being out of date for pip install on older macos machine
    
    * ios fix attempts for old build system on travis
    
    * update python version for pip install
    
    * disabled ios framework installs until we can figure out why this fails in latest travis pipeline
    TODO: fix framework installs for ios

--- source/scripts/android_build.sh
+++ source/scripts/android_build.sh
@@ -22,13 +22,13 @@ if [[ "$PWD" == *source ]]
 then
 	echo "Script called from correct path: $PWD"
 	echo "Compiling Static Android libs..."
-	cmake . -B_builds/android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_ANDROID_STL_TYPE=gnustl_static -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx
+	cmake . -B_builds/android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_ANDROID_STL_TYPE=gnustl_static -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx
 	cmake --build _builds/android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx --config Release --target install
-	cmake . -B_builds/android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_ANDROID_STL_TYPE=gnustl_static -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx
+	cmake . -B_builds/android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_ANDROID_STL_TYPE=gnustl_static -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx
 	cmake --build _builds/android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx --config Release --target install
-	cmake . -B_builds/android-ndk-r16b-api-21-x86-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=x86 -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_ANDROID_STL_TYPE=gnustl_static -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-x86-clang-libcxx
+	cmake . -B_builds/android-ndk-r16b-api-21-x86-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=x86 -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_ANDROID_STL_TYPE=gnustl_static -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-x86-clang-libcxx
 	cmake --build _builds/android-ndk-r16b-api-21-x86-clang-libcxx --config Release --target install
-	cmake . -B_builds/android-ndk-r16b-api-21-x86-64-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=x86_64 -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_ANDROID_STL_TYPE=gnustl_static -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-x86-64-clang-libcxx
+	cmake . -B_builds/android-ndk-r16b-api-21-x86-64-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=x86_64 -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_ANDROID_STL_TYPE=gnustl_static -DBUILD_SHARED_LIBS=1 -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-x86-64-clang-libcxx
 	cmake --build _builds/android-ndk-r16b-api-21-x86-64-clang-libcxx --config Release --target install
 	cd ${TRAVIS_BUILD_DIR}/source
 	mv _install/android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx _install/android-armeabi-v7a
@@ -52,28 +52,6 @@ then
 	rsync -c "_install/android-x86_64/lib/libMach1EncodeCAPI.a" "../mach1spatial-libs/android-x86_64/lib/libMach1EncodeCAPI.a"
 	rsync -c "_install/android-x86_64/lib/libMach1TranscodeCAPI.a" "../mach1spatial-libs/android-x86_64/lib/libMach1TranscodeCAPI.a"
 	rsync -c "_install/android-x86_64/lib/libMach1DecodePositionalCAPI.a" "../mach1spatial-libs/android-x86_64/lib/libMach1DecodePositionalCAPI.a"
-	# Upload built static libs
-	if [[ $upload_artifacts == "ON" ]]
-	then
-		aws s3 sync _install/android-armeabi-v7a/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/android-armeabi-v7a/lib --exclude "*-minifiedCAPI*" --cache-control no-cache
-		aws s3 sync _install/android-arm64-v8a/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/android-arm64-v8a/lib --exclude "*-minifiedCAPI*" --cache-control no-cache
-		aws s3 sync _install/android-x86/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/android-x86/lib --exclude "*-minifiedCAPI*" --cache-control no-cache
-		aws s3 sync _install/android-x86_64/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/android-x86_64/lib --exclude "*-minifiedCAPI*" --cache-control no-cache
-	fi
-	echo "Compiling Shared Android libs..."
-	cmake . -B_builds/android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx
-	cmake --build _builds/android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx --config Release --target install
-	cmake . -B_builds/android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx
-	cmake --build _builds/android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx --config Release --target install
-	cmake . -B_builds/android-ndk-r16b-api-21-x86-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=x86 -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-x86-clang-libcxx
-	cmake --build _builds/android-ndk-r16b-api-21-x86-clang-libcxx --config Release --target install
-	cmake . -B_builds/android-ndk-r16b-api-21-x86-64-clang-libcxx -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=x86_64 -DCMAKE_ANDROID_NDK=${ANDROID_NDK_r16b} -DCMAKE_INSTALL_PREFIX=`pwd`/_install/android-ndk-r16b-api-21-x86-64-clang-libcxx
-	cmake --build _builds/android-ndk-r16b-api-21-x86-64-clang-libcxx --config Release --target install
-	cd ${TRAVIS_BUILD_DIR}/source
-	mv _install/android-ndk-r16b-api-21-armeabi-v7a-neon-clang-libcxx _install/android-armeabi-v7a
-	mv _install/android-ndk-r16b-api-21-arm64-v8a-neon-clang-libcxx _install/android-arm64-v8a
-	mv _install/android-ndk-r16b-api-21-x86-clang-libcxx _install/android-x86
-	mv _install/android-ndk-r16b-api-21-x86-64-clang-libcxx _install/android-x86_64
 	# INSTALL SHARED ANDROID LIBS
 	rsync -c "_install/android-armeabi-v7a/lib-shared/libMach1DecodeCAPI.so" "../mach1spatial-libs/android-armeabi-v7a/lib-shared/libMach1DecodeCAPI.so"
 	rsync -c "_install/android-armeabi-v7a/lib-shared/libMach1EncodeCAPI.so" "../mach1spatial-libs/android-armeabi-v7a/lib-shared/libMach1EncodeCAPI.so"
@@ -91,9 +69,14 @@ then
 	rsync -c "_install/android-x86_64/lib-shared/libMach1EncodeCAPI.so" "../mach1spatial-libs/android-x86_64/lib-shared/libMach1EncodeCAPI.so"
 	rsync -c "_install/android-x86_64/lib-shared/libMach1TranscodeCAPI.so" "../mach1spatial-libs/android-x86_64/lib-shared/libMach1TranscodeCAPI.so"
 	rsync -c "_install/android-x86_64/lib-shared/libMach1DecodePositionalCAPI.so" "../mach1spatial-libs/android-x86_64/lib-shared/libMach1DecodePositionalCAPI.so"
-	# Upload built shared libs
+	# Upload built static libs
 	if [[ $upload_artifacts == "ON" ]]
 	then
+		aws s3 sync _install/android-armeabi-v7a/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/android-armeabi-v7a/lib --exclude "*-minifiedCAPI*" --cache-control no-cache
+		aws s3 sync _install/android-arm64-v8a/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/android-arm64-v8a/lib --exclude "*-minifiedCAPI*" --cache-control no-cache
+		aws s3 sync _install/android-x86/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/android-x86/lib --exclude "*-minifiedCAPI*" --cache-control no-cache
+		aws s3 sync _install/android-x86_64/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/android-x86_64/lib --exclude "*-minifiedCAPI*" --cache-control no-cache
+		# shared libs
 		aws s3 sync _install/android-armeabi-v7a/lib-shared/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/android-armeabi-v7a/lib-shared --exclude "*-minifiedCAPI*" --cache-control no-cache
 		aws s3 sync _install/android-arm64-v8a/lib-shared/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/android-arm64-v8a/lib-shared --exclude "*-minifiedCAPI*" --cache-control no-cache
 		aws s3 sync _install/android-x86/lib-shared/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/android-x86/lib-shared --exclude "*-minifiedCAPI*" --cache-control no-cache
--- source/scripts/ios_build.sh
+++ source/scripts/ios_build.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 
+while getopts 'uh:' OPTION; do
+	case "$OPTION" in
+		u)
+			upload_artifacts=ON
+			echo "Uploading binaries to aws"
+			;;
+		h)
+			echo "script usage: $(basename \$0) [-u] [-h]" >&2
+			exit 1
+			;;
+		?)
+			echo "script usage: $(basename \$0) [-u] [-h]" >&2
+			exit 1
+			;;
+	esac
+done
+# shift "$(($OPTION -1))"
+
 if [[ "$PWD" == *source ]]
 then
 	echo "Script called from correct path: $PWD"
 	# IOS BUILDS
 	cmake . -B_builds/ios-11-3-dep-9-0-device-bitcode -GXcode -T buildsystem=1 -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_DEPLOYMENT_TARGET=9.3 -DCMAKE_INSTALL_PREFIX=`pwd`/_install/ios-11-3-dep-9-0-device-bitcode -DCMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=NO -DCMAKE_IOS_INSTALL_COMBINED=YES
 	cmake --build _builds/ios-11-3-dep-9-0-device-bitcode --config Release --target install
+	mkdir -p "../mach1spatial-libs/ios/lib"
 	rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/lib/libMach1DecodeCAPI.a" "../mach1spatial-libs/ios/lib/libMach1DecodeCAPI.a"
 	rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/lib/libMach1EncodeCAPI.a" "../mach1spatial-libs/ios/lib/libMach1EncodeCAPI.a"
 	rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/lib/libMach1DecodePositionalCAPI.a" "../mach1spatial-libs/ios/lib/libMach1DecodePositionalCAPI.a"
 	rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/lib/libMach1TranscodeCAPI.a" "../mach1spatial-libs/ios/lib/libMach1TranscodeCAPI.a"
-	rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/Frameworks/libMach1DecodeCAPI.framework" "../mach1spatial-libs/ios/Frameworks/libMach1DecodeCAPI.framework"
-	rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/Frameworks/libMach1EncodeCAPI.framework" "../mach1spatial-libs/ios/FrameworksFrameworks/libMach1EncodeCAPI.framework"
-	rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/Frameworks/libMach1DecodePositionalCAPI.framework" "../mach1spatial-libs/ios/Frameworks/libMach1DecodePositionalCAPI.framework"
-	rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/Frameworks/libMach1TranscodeCAPI.framework" "../mach1spatial-libs/ios/Frameworks/libMach1TranscodeCAPI.framework"
+	# TODO: Disable framework installs until we discover why they dont build in latest Travis
+	# rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/Frameworks/libMach1DecodeCAPI.framework" "../mach1spatial-libs/ios/Frameworks/libMach1DecodeCAPI.framework"
+	# rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/Frameworks/libMach1EncodeCAPI.framework" "../mach1spatial-libs/ios/FrameworksFrameworks/libMach1EncodeCAPI.framework"
+	# rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/Frameworks/libMach1DecodePositionalCAPI.framework" "../mach1spatial-libs/ios/Frameworks/libMach1DecodePositionalCAPI.framework"
+	# rsync -c "_install/ios-11-3-dep-9-0-device-bitcode/Frameworks/libMach1TranscodeCAPI.framework" "../mach1spatial-libs/ios/Frameworks/libMach1TranscodeCAPI.framework"
 	rsync -c include/swift/* ../examples/mach1spatial-c/ios/Pod-Mach1SpatialAPI/Mach1SpatialAPI/Classes
 	# Upload built libs
-	aws s3 sync _install/ios-11-3-dep-9-0-device-bitcode/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/ios/lib --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
+	if [[ $upload_artifacts == "ON" ]]
+	then
+		aws s3 sync _install/ios-11-3-dep-9-0-device-bitcode/lib/ s3://${AWS_DEPLOY_BUCKET}/mach1spatial-libs/ios/lib --exclude "*-minifiedCAPI*" --cache-control no-cache --metadata-directive REPLACE
+	fi
 else
 	echo "ERROR: Script called from wrong directory: $PWD"
 fi

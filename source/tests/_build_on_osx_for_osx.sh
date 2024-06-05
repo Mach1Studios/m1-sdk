ac2354cf9f557c88a7147a7476219825757eebb7
Dylan Marcus <dylan@dylanjmarcus.com>
2024-06-05 15:00:42 -0400

    exit(1) tests if any fail for build pipeline

--- source/tests/_build_on_osx_for_osx.sh
+++ source/tests/_build_on_osx_for_osx.sh
@@ -5,11 +5,11 @@ echo "### BUILD macOS ###"
 cmake . -B_builds/xcode -GXcode -DCMAKE_INSTALL_PREFIX=`pwd`/_install/xcode
 cmake --build _builds/xcode --target install
 
-_install/xcode/bin/Mach1EncodeTests
-_install/xcode/bin/Mach1DecodeTests
-_install/xcode/bin/Mach1DecodePositionalTests
-_install/xcode/bin/Mach1TranscodeTests
-_install/xcode/bin/Mach1SpatialTests
+_install/xcode/bin/Mach1EncodeTests || { echo 'Mach1Encode API test failed...' ; exit 1; }
+_install/xcode/bin/Mach1DecodeTests || { echo 'Mach1Decode API test failed...' ; exit 1; }
+_install/xcode/bin/Mach1DecodePositionalTests || { echo 'Mach1DecodePositional API test failed...' ; exit 1; }
+_install/xcode/bin/Mach1TranscodeTests || { echo 'Mach1Transcode API test failed...' ; exit 1; }
+_install/xcode/bin/Mach1SpatialTests || { echo 'Additional API tests failed...' ; exit 1; }
 
 echo "### RENDER CHECK ####"
 echo "### 5.1_C -> M1Spatial ###"

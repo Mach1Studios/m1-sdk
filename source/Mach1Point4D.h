89ca895025c4679612efdc3c2ce700cd75d7e442
Dylan Marcus <dylan@dylanjmarcus.com>
2024-07-22 16:34:03 -0400

    combine classes into a single header for the point classes

--- source/Mach1Point4D.h
+++ source/Mach1Point4D.h
@@ -18,22 +18,55 @@
 #endif
 
 #ifdef __cplusplus
+
 class Mach1Point4D {
   public:
     float x, y, z, w;
 
-    Mach1Point4D();
-    Mach1Point4D(float X, float Y, float Z, float W);
+    Mach1Point4D() {
+        x = 0;
+        y = 0;
+        z = 0;
+        w = 0;
+    }
 
-    Mach1Point4D operator+(const Mach1Point4D &pnt) const;
-    Mach1Point4D operator*(const float f) const;
-    Mach1Point4D operator*(const Mach1Point4D &vec) const;
-    Mach1Point4D operator-(const Mach1Point4D &vec) const;
-    float length() const;
-    float operator[](int index);
+    Mach1Point4D(float X, float Y, float Z, float W) {
+        x = X;
+        y = Y;
+        z = Z;
+        w = W;
+    }
+
+    Mach1Point4D operator+(const Mach1Point4D &pnt) const {
+        return Mach1Point4D(x + pnt.x, y + pnt.y, z + pnt.z, w + pnt.w);
+    }
+
+    Mach1Point4D operator*(const float f) const {
+        return Mach1Point4D(x * f, y * f, z * f, w * f);
+    }
+
+    Mach1Point4D operator*(const Mach1Point4D &vec) const {
+        return Mach1Point4D(x * vec.x, y * vec.y, z * vec.z, w * vec.w);
+    }
+
+    Mach1Point4D operator-(const Mach1Point4D &vec) const {
+        return Mach1Point4D(x - vec.x, y - vec.y, z - vec.z, w - vec.w);
+    }
+
+    float length() const {
+        return (float)sqrtf(x * x + y * y + z * z + w * w);
+    }
+
+    float operator[](int index) {
+        float arr[4] = {x, y, z, w};
+        return arr[index];
+    }
 };
+
 #else
+
 struct Mach1Point4D {
     float x, y, z, w;
 };
+
 #endif

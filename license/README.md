bdccff40b2eb739afcbc6ce9bb84133d4c170056
Dylan Marcus <dylan@mach1studios.com>
2025-02-07 15:57:11 -0500

    Feature/license update (#62)
    * stripping away some old licenses and starting the new summary
    
    * update license source files for 3.0 open use license
    
    * license source files generate distributables
    
    * removed unneeded step
    
    * add license with background
    
    * typo
    
    * Update Mach1-LicenseSummary.txt
    
    * remove old file, update png and script
    
    * fixed automated license scripting including the summary setup
    
    * update license after review
    
    * spacing fixes to docx for header
    
    ---------
    
    Co-authored-by: Meeker <hmeeker@heathermeeker.com>

--- license/README.md
+++ license/README.md
@@ -1,16 +1,8 @@
-## Mach1 Spatial SDK Royalty Free License ##
-
-***IMPORTANT NOTICE: PLEASE READ CAREFULLY BEFORE DOWNLOADING, INSTALLING OR USING THE MACH1 SPATIAL SDK:***
- - BY CLICKING ON THE “ACCEPT” BUTTON, OR BY DOWNLOADING, INSTALLING, OR USING THE MACH1 SPATIAL SDK, YOU AGREE TO THE TERMS OF THIS AGREEMENT, AND ALL INCORPORATED TERMS INCLUDING THE PRIVACY POLICY, AS APPLICABLE, WHICH WILL BIND YOU.
-- YOU MUST JOIN THE SPATIALAUDIO.SLACK.COM WORKSPACE TO KEEP IN TOUCH ABOUT THIS LICENSE AND MOST RECENT FEATURES ABOUT THE MACH1 SPATIAL SDK.
-- The most current version of the MACH1 SPATIAL SDK ROYALTY FREE LICENSE will be posted at www.mach1.tech/license. If the Current Version has a more recent effective date than this document, then this document is replaced by the Currnt Version and by clicking on the appropriate button below, or by downloading, installing, activating or using any of the SDK, or any other update method Mach1 may choose, you agree to be bound by the Current Version. You may not use an old version of any SDK if you have not agreed to the Current Version.
- - IF YOU DO NOT AGREE TO THE TERMS OF THIS AGREEMENT, WE WILL NOT LICENSE THE MACH1 SPATIAL SDK TO YOU AND YOU MUST DISCONTINUE THE INSTALLATION OR DOWNLOAD PROCESS OR CEASE USE OF THE MACH1 SPATIAL SDK.
+## Mach1 Spatial SDK Open Use License ##
 
 ### SUMMARY ###
-![Mach1 Spatial SDK Royalty Free License](Mach1-LicenseDiagram.png "Mach1 Spatial SDK Royalty Free License")
+![Mach1 Spatial SDK Open Use License](Mach1-LicenseSummary-wbg.png "Mach1 Spatial SDK Open Use License")
 
-By downloading and/or using the Mach1 Spatial SDK, including use of any of the contents within the [binaries/](../binaries),
-you agree to and acknowledge the terms of use set forth by the [Mach1 Spatial SDK License](https://www.mach1.tech/license).
 If you do not agree to the terms set forth by the [Mach1 Spatial SDK License](https://www.mach1.tech/license) you are not
 permitted to use, link, compile and/or distribute any of the contents of this repository.
 
--- license/distribute_license.sh
+++ license/distribute_license.sh
@@ -4,55 +4,55 @@
 # Mach1 INTERNAL
 # Properly copy and propogate all license files to this repo and to the release repo
 # 
+# Setup: 
+#  - Install Libreoffice
+#  - `brew install unoconvert libreoffice`
+#  - `pip install psd-tools`
+#
 # Please complete the following first:
 #  - Update the license version number
-#  - Export a pdf named with version
-#  - Create a plain text version and replace LICENSE.txt
-#  - Create html version of LICENSE.txt
 #
+
+# Generate files
+python process_summary.py
+unoconv -f pdf Mach1SpatialSDK-OpenUseLicense.docx
+soffice --headless --convert-to "txt:Text (encoded):UTF8" Mach1SpatialSDK-OpenUseLicense.docx
+cat Mach1-LicenseSummary.txt Mach1SpatialSDK-OpenUseLicense.txt > _Mach1SpatialSDK-OpenUseLicense.txt
+mv _Mach1SpatialSDK-OpenUseLicense.txt Mach1SpatialSDK-OpenUseLicense.txt
+mv Mach1SpatialSDK-OpenUseLicense.txt LICENSE.txt
+soffice --headless --convert-to "html:XHTML Writer File:UTF8" --convert-images-to "jpg" Mach1SpatialSDK-OpenUseLicense.docx 
+mv Mach1SpatialSDK-OpenUseLicense.html LICENSE.html
 
 # copy to repo places
 cp LICENSE.txt ../LICENSE.txt
-cp LICENSE.txt ../mach1spatial-libs/LICENSE.txt
 cp LICENSE.txt ../examples/mach1spatial-c/android/JitPack-Mach1SpatialAPI/LICENSE.txt
 cp LICENSE.txt ../examples/mach1spatial-c/ios/Pod-Mach1SpatialAPI/LICENSE.txt
 
-cp LICENSE.txt ../examples/mach1spatial-c/Unity/M1UnityDecodeTest/Assets/Mach1/license/LICENSE.txt
-cp Mach1SpatialSDK-RoyaltyFreeLicense.pdf ../examples/mach1spatial-c/Unity/M1UnityDecodeTest/Assets/Mach1/license/Mach1SpatialSDK-RoyaltyFreeLicense.pdf
-cp -r attribution ../examples/mach1spatial-c/Unity/M1UnityDecodeTest/Assets/Mach1/license/
-cp LICENSE.txt ../examples/mach1spatial-c/Unity/M1UnityDecodeTest/Assets/Mach1/Plugins/LICENSE.txt
+cp LICENSE.txt ../examples/mach1spatial-c/Unity/Unity-Mach1SpatialAPI/M1UnityDecode/Assets/Mach1/license/LICENSE.txt
+cp Mach1SpatialSDK-OpenUseLicense.pdf ../examples/mach1spatial-c/Unity/Unity-Mach1SpatialAPI/M1UnityDecode/Assets/Mach1/license/Mach1SpatialSDK-OpenUseLicense.pdf
+cp -r attribution ../examples/mach1spatial-c/Unity/Unity-Mach1SpatialAPI/M1UnityDecode/Assets/Mach1/license/
+cp LICENSE.txt ../examples/mach1spatial-c/Unity/Unity-Mach1SpatialAPI/M1UnityDecode/Assets/Mach1/Plugins/LICENSE.txt
 
-cp LICENSE.txt ../examples/mach1spatial-c/Unity/DSPGraph/Assets/Mach1/license/LICENSE.txt
-cp Mach1SpatialSDK-RoyaltyFreeLicense.pdf ../examples/mach1spatial-c/Unity/DSPGraph/Assets/Mach1/license/Mach1SpatialSDK-RoyaltyFreeLicense.pdf
-cp -r attribution ../examples/mach1spatial-c/Unity/DSPGraph/Assets/Mach1/license/
-cp LICENSE.txt ../examples/mach1spatial-c/Unity/DSPGraph/Assets/Mach1/Plugins/LICENSE.txt
+cp LICENSE.txt ../examples/mach1spatial-c/Unity/Unity-Mach1SpatialAPI/M1DSPGraph/Assets/Mach1/license/LICENSE.txt
+cp Mach1SpatialSDK-OpenUseLicense.pdf ../examples/mach1spatial-c/Unity/Unity-Mach1SpatialAPI/M1DSPGraph/Assets/Mach1/license/Mach1SpatialSDK-OpenUseLicense.pdf
+cp -r attribution ../examples/mach1spatial-c/Unity/Unity-Mach1SpatialAPI/M1DSPGraph/Assets/Mach1/license/
+cp LICENSE.txt ../examples/mach1spatial-c/Unity/Unity-Mach1SpatialAPI/M1DSPGraph/Assets/Mach1/Plugins/LICENSE.txt
 
-cp LICENSE.txt ../examples/mach1spatial-c/Unreal\ Engine/Mach1DecodePlugin/Resources/license/LICENSE.txt
-cp Mach1SpatialSDK-RoyaltyFreeLicense.pdf ../examples/mach1spatial-c/Unreal\ Engine/Mach1DecodePlugin/Resources/license/Mach1SpatialSDK-RoyaltyFreeLicense.pdf
-cp -r attribution ../examples/mach1spatial-c/Unreal\ Engine/Mach1DecodePlugin/Resources/license/
+cp LICENSE.txt ../examples/mach1spatial-c/Unreal\ Engine/UE-Mach1SpatialAPI/Mach1DecodePlugin/Resources/license/LICENSE.txt
+cp Mach1SpatialSDK-OpenUseLicense.pdf ../examples/mach1spatial-c/Unreal\ Engine/UE-Mach1SpatialAPI/Mach1DecodePlugin/Resources/license/Mach1SpatialSDK-OpenUseLicense.pdf
+cp -r attribution ../examples/mach1spatial-c/Unreal\ Engine/UE-Mach1SpatialAPI/Mach1DecodePlugin/Resources/license/
 
 cp LICENSE.txt ../examples/mach1spatial-web/js/LICENSE.txt
 cp LICENSE.txt ../examples/mach1spatial-web/m1-web-spatialaudioplayer/js/LICENSE.txt
 
-# copy to release places
-cp LICENSE.txt ../../m1-sdk-release/LICENSE.txt
-cp LICENSE.txt ../../m1-sdk-release/mach1spatial-libs/LICENSE.txt
-
-cp LICENSE.txt ../../m1-sdk-release/examples/mach1spatial-web/js/LICENSE.txt
-
-cp LICENSE.txt ../../m1-sdk-release/license/LICENSE.txt
-cp Mach1SpatialSDK-RoyaltyFreeLicense.pdf ../../m1-sdk-release/license/Mach1SpatialSDK-RoyaltyFreeLicense.pdf
-cp Mach1-LicenseDiagram.png ../../m1-sdk-release/license/Mach1-LicenseDiagram.png
-cp -r attribution ../../m1-sdk-release/license/
-
 # copy to website and s3
 rm "Mach1SpatialSDK-License.zip"
-zip Mach1SpatialSDK-License.zip -r . -x 'Mach1-LicenseDiagram.graffle/*' -x '*.docx' -x 'Mach1-LicenseDiagram.png' -x '*.DS_Store' -x '*.sh' -x '*.html' -x 'Mach1-LicenseDiagram_nobug.png' -x '*.ai' -x 'attribution/Mach1-EndUserLicenseAgreement-General.docx' -x '.gitignore'
+zip Mach1SpatialSDK-License.zip -r . -x '*.docx' -x 'Mach1-Mach1-LicenseSummary.png' -x '*.DS_Store' -x '*.sh' -x '*.html' -x 'Mach1-LicenseDiagram_nobug.png' -x '.gitignore'
 
 aws s3 cp "LICENSE.txt" "s3://mach1-public/assets/license/LICENSE.txt" --cache-control 'max-age 0,no-cache,no-store,must-revalidate' --metadata-directive REPLACE --profile mach1
 aws s3 cp "LICENSE.html" "s3://mach1-public/assets/license/LICENSE.html" --cache-control 'max-age 0,no-cache,no-store,must-revalidate' --metadata-directive REPLACE --profile mach1
-aws s3 cp "Mach1SpatialSDK-RoyaltyFreeLicense.pdf" "s3://mach1-public/assets/license/Mach1SpatialSDK-RoyaltyFreeLicense.pdf" --cache-control 'max-age 0,no-cache,no-store,must-revalidate' --metadata-directive REPLACE --profile mach1
-aws s3 cp "Mach1-LicenseDiagram.png" "s3://mach1-public/assets/license/Mach1-LicenseDiagram.png" --cache-control 'max-age 0,no-cache,no-store,must-revalidate' --metadata-directive REPLACE --profile mach1
-aws s3 cp "Mach1-LicenseDiagram-wbg.png" "s3://mach1-public/assets/license/Mach1-LicenseDiagram-wbg.png" --cache-control 'max-age 0,no-cache,no-store,must-revalidate' --metadata-directive REPLACE --profile mach1
+aws s3 cp "Mach1SpatialSDK-OpenUseLicense.pdf" "s3://mach1-public/assets/license/Mach1SpatialSDK-OpenUseLicense.pdf" --cache-control 'max-age 0,no-cache,no-store,must-revalidate' --metadata-directive REPLACE --profile mach1
+aws s3 cp "Mach1-Mach1-LicenseSummary.png" "s3://mach1-public/assets/license/Mach1-Mach1-LicenseSummary.png" --cache-control 'max-age 0,no-cache,no-store,must-revalidate' --metadata-directive REPLACE --profile mach1
+aws s3 cp "Mach1-Mach1-LicenseSummary-wbg.png" "s3://mach1-public/assets/license/Mach1-Mach1-LicenseSummary-wbg.png" --cache-control 'max-age 0,no-cache,no-store,must-revalidate' --metadata-directive REPLACE --profile mach1
 aws s3 cp "attribution/Mach1-EndUserLicenseAgreement-General.pdf" "s3://mach1-public/assets/license/Mach1-EndUserLicenseAgreement-General.pdf" --cache-control 'max-age 0,no-cache,no-store,must-revalidate' --metadata-directive REPLACE --profile mach1
 aws s3 cp "Mach1SpatialSDK-License.zip" "s3://mach1-releases/license/Mach1SpatialSDK-License.zip" --cache-control 'max-age 0,no-cache,no-store,must-revalidate' --metadata-directive REPLACE --profile mach1
